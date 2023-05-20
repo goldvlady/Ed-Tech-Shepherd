b50ee8e8a2fa55409033f16c0edb90f8d16ac7e3
goldvlady <vladyslavabramov@outlook.com>
2023-05-21 00:04:18 +0100

    Tutor Profile View

--- src/views/Signup.tsx
+++ src/views/Signup.tsx
@@ -1,108 +1,208 @@
-import { Box, Button, FormControl, FormErrorMessage, FormLabel, Heading, Input, Link, Text, useToast } from '@chakra-ui/react';
-import React from 'react';
-import { Link as RouterLink, useNavigate } from "react-router-dom"
-import styled from 'styled-components';
-import SecureInput from '../components/SecureInput';
-import { Field, Form, Formik } from 'formik';
-import * as Yup from 'yup';
-import CriteriaCheck from '../components/CriteriaCheck';
-import { MinPasswordLength } from '../util';
-import { useTitle } from '../hooks';
-import { createUserWithEmailAndPassword, firebaseAuth } from '../firebase';
+import React, { useState, useEffect, forwardRef, useRef } from 'react';
+import { Link } from 'react-router-dom';
+import { HiEye, HiEyeOff } from 'react-icons/hi';
+import {
+    Flex,
+    Box,
+    ButtonGroup,
+    VisuallyHidden,
+    createIcon,
+    FormControl,
+    FormLabel,
+    Input,
+    Checkbox,
+    Stack,
+    Container,
+    Divider,
+    Button,
+    Heading,
+    Text,
+    HStack,
+    IconButton,
+    InputGroup,
+    InputProps,
+    InputRightElement,
+    useDisclosure,
+    useMergeRefs,
+    useColorModeValue,
+} from '@chakra-ui/react';
+import { firebaseAuth } from '../firebase';
+import Logo from "../components/Logo";
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword, GoogleAuthProvider, getAuth, signInWithPopup, } from 'firebase/auth';
+import { String } from 'lodash';
 
-const Root = styled(Box)`
-`
 
-const SignupSchema = Yup.object().shape({
-    email: Yup.string().email('Enter a valid email address').required('A valid email address is required'),
-    password: Yup.string().required('A password is required').min(MinPasswordLength, `Your password must be a minimum of ${MinPasswordLength} characters`),
-    passwordConfirmation: Yup.string()
-        .test('passwords-match', 'Passwords must match', function (value) {
-            return this.parent.password === value
-        })
-});
+function SignUp() {
+    const [email, setEmail] = useState<string>("akinola_oladapo@yahoo.com");
+    const [password, setPassword] = useState<string>("pass1234$$");
 
-const Signup: React.FC = () => {
-    useTitle('Sign up');
-    const toast = useToast();
-    const navigate = useNavigate();
-    return <Root>
-        <Box mb={'20px'}>
-            <Heading mb={'12px'} as={'h3'} textAlign={"center"}>Create your Shepherd Account</Heading>
-            <Text m={0} className='body2' textAlign={"center"}>Hi there, before you proceed, let us know who is signing up</Text>
-        </Box>
-        <Box>
-            <Formik
-                initialValues={{ email: '', password: '', passwordConfirmation: '' }}
-                validationSchema={SignupSchema}
-                onSubmit={async (values, { setSubmitting }) => {
-                    try {
-                        await createUserWithEmailAndPassword(firebaseAuth, values.email, values.password);
-                        navigate('/dashboard');
-                    } catch (e: any) {
-                        let errorMessage = '';
-                        switch (e.code) {
-                            case 'auth/email-already-exists':
-                                errorMessage = "A user with this email address already exists";
-                                break;
-                            default:
-                                errorMessage = "An unexpected error occurred";
-                                break;
-                        }
 
-                        toast({
-                            title: errorMessage,
-                            position: 'top-right',
-                            status: 'error',
-                            isClosable: true,
-                        })
-                    }
-                    setSubmitting(false);
-                }}
+
+    const googleProvider = new GoogleAuthProvider();
+    const signInWithGoogle = async () => {
+        signInWithPopup(firebaseAuth, googleProvider)
+            .then((result) => {
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                const credential = GoogleAuthProvider.credentialFromResult(result);
+                const token = credential?.accessToken;
+                // The signed-in user info.
+                const user = result.user;
+                console.log(user);
+
+                // IdP data available using getAdditionalUserInfo(result)
+                // ...
+            }).catch((error) => {
+                // Handle Errors here.
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // The email of the user's account used.
+                const email = error.customData.email;
+                // The AuthCredential type that was used.
+                const credential = GoogleAuthProvider.credentialFromError(error);
+                // ...
+            });
+    };
+
+
+
+    const logInWithEmailAndPassword = async () => {
+        try {
+            await signInWithEmailAndPassword(firebaseAuth, email, password);
+        } catch (err: any) {
+            console.error(err);
+            alert(err.message);
+        }
+    };
+    // useEffect(() => {
+    //     signInWithGoogle();
+    // }, [])
+
+    const PasswordField = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
+        const { isOpen, onToggle } = useDisclosure()
+        const inputRef = useRef<HTMLInputElement>(null)
+
+        const mergeRef = useMergeRefs(inputRef, ref)
+        const onClickReveal = () => {
+            onToggle()
+            if (inputRef.current) {
+                inputRef.current.focus({ preventScroll: true })
+            }
+        }
+
+        return (
+            <FormControl>
+                <FormLabel htmlFor="password">Password</FormLabel>
+                <InputGroup>
+                    <InputRightElement>
+                        <IconButton
+                            variant="link"
+                            aria-label={isOpen ? 'Mask password' : 'Reveal password'}
+                            icon={isOpen ? <HiEyeOff /> : <HiEye />}
+                            onClick={onClickReveal}
+                        />
+                    </InputRightElement>
+                    <Input
+                        id="password"
+                        ref={mergeRef}
+                        name="password"
+                        type={isOpen ? 'text' : 'password'}
+                        autoComplete="current-password"
+                        required
+                        {...props}
+                    />
+                </InputGroup>
+            </FormControl>
+        )
+    })
+
+    const GoogleIcon = createIcon({
+        displayName: 'GoogleIcon',
+        path: (
+            <g transform="matrix(1, 0, 0, 1, 27.009001, -39.238998)">
+                <path
+                    fill="#4285F4"
+                    d="M -3.264 51.509 C -3.264 50.719 -3.334 49.969 -3.454 49.239 L -14.754 49.239 L -14.754 53.749 L -8.284 53.749 C -8.574 55.229 -9.424 56.479 -10.684 57.329 L -10.684 60.329 L -6.824 60.329 C -4.564 58.239 -3.264 55.159 -3.264 51.509 Z"
+                />
+                <path
+                    fill="#34A853"
+                    d="M -14.754 63.239 C -11.514 63.239 -8.804 62.159 -6.824 60.329 L -10.684 57.329 C -11.764 58.049 -13.134 58.489 -14.754 58.489 C -17.884 58.489 -20.534 56.379 -21.484 53.529 L -25.464 53.529 L -25.464 56.619 C -23.494 60.539 -19.444 63.239 -14.754 63.239 Z"
+                />
+                <path
+                    fill="#FBBC05"
+                    d="M -21.484 53.529 C -21.734 52.809 -21.864 52.039 -21.864 51.239 C -21.864 50.439 -21.724 49.669 -21.484 48.949 L -21.484 45.859 L -25.464 45.859 C -26.284 47.479 -26.754 49.299 -26.754 51.239 C -26.754 53.179 -26.284 54.999 -25.464 56.619 L -21.484 53.529 Z"
+                />
+                <path
+                    fill="#EA4335"
+                    d="M -14.754 43.989 C -12.984 43.989 -11.404 44.599 -10.154 45.789 L -6.734 42.369 C -8.804 40.429 -11.514 39.239 -14.754 39.239 C -19.444 39.239 -23.494 41.939 -25.464 45.859 L -21.484 48.949 C -20.534 46.099 -17.884 43.989 -14.754 43.989 Z"
+                />
+            </g>
+        ),
+    })
+
+    const providers = [
+        { name: 'Google', icon: <GoogleIcon boxSize="5" /> },
+    ]
+
+
+
+    return (<Container maxW="lg" py={{ base: '12', md: '24' }} px={{ base: '0', sm: '8' }}>
+        <Stack spacing="8">
+            <Stack spacing="6">
+                <Box textAlign={"center"}><Logo /></Box>
+                <Stack spacing={{ base: '2', md: '3' }} textAlign="center">
+                    <Heading size={{ base: 'xs', md: 'sm' }}>Register your account</Heading>
+                    <HStack spacing="1" justify="center">
+                        <Text color="muted">Already have an account?</Text>
+                        <Button variant="link" colorScheme="blue">
+                            <Link to="/login"> Log in</Link>
+
+                        </Button>
+                    </HStack>
+                </Stack>
+            </Stack>
+            <Box
+                py={{ base: '0', sm: '8' }}
+                px={{ base: '4', sm: '10' }}
+                bg={{ base: 'transparent', sm: 'bg-surface' }}
+                boxShadow={{ base: 'none', sm: 'md' }}
+                borderRadius={{ base: 'none', sm: 'xl' }}
             >
-                {({
-                    errors,
-                    isSubmitting,
-                    values
-                }) => (
-                    <Form>
-                        <Field name='email'>
-                            {({ field, form }: { field: any, form: any }) => (
-                                <FormControl isInvalid={form.errors.email && form.touched.email}>
-                                    <FormLabel>Email</FormLabel>
-                                    <Input size={'lg'} isInvalid={form.errors.email && form.touched.email} {...field} placeholder='Enter your email' />
-                                    <FormErrorMessage>{form.errors.email}</FormErrorMessage>
-                                </FormControl>
-                            )}
-                        </Field>
-                        <Field name='password'>
-                            {({ field, form }: { field: any, form: any }) => (
-                                <FormControl marginTop={'22px'} isInvalid={form.errors.password && form.touched.password}>
-                                    <FormLabel>Password</FormLabel>
-                                    <SecureInput size={'lg'} isInvalid={form.errors.password && form.touched.password} {...field} placeholder='Enter password' />
-                                    <FormErrorMessage>{form.errors.password}</FormErrorMessage>
-                                </FormControl>
-                            )}
-                        </Field>
-                        <Field name='passwordConfirmation'>
-                            {({ field, form }: { field: any, form: any }) => (
-                                <FormControl marginTop={'22px'} isInvalid={form.errors.passwordConfirmation && form.touched.passwordConfirmation}>
-                                    <FormLabel>Confirm Password</FormLabel>
-                                    <SecureInput size={'lg'} isInvalid={form.errors.passwordConfirmation && form.touched.passwordConfirmation} {...field} placeholder='Re-enter password' />
-                                    <FormErrorMessage>{form.errors.passwordConfirmation}</FormErrorMessage>
-                                </FormControl>
-                            )}
-                        </Field>
-                        <CriteriaCheck mt={17.5} text={`${MinPasswordLength} character minimum`} checked={values.password.length >= MinPasswordLength} />
-                        <Box marginTop={'36px'} display={"flex"} flexDirection="column" gap={4} justifyContent="flex-end">
-                            <Button isDisabled={Object.values(errors).length !== 0} width={'100%'} size='lg' type='submit' isLoading={isSubmitting}>Sign Up</Button>
-                            <Link color='primary.400' className='body2 text-center' as={RouterLink} to='/login'><span className='body2'>Already have an account?</span> Login</Link>
-                        </Box>
-                    </Form>
-                )}
-            </Formik>
-        </Box>
-    </Root>
+                <Stack spacing="6">
+                    {/* <Stack spacing="5">
+                        <FormControl>
+                            <FormLabel htmlFor="email">Email</FormLabel>
+                            <Input id="email" type="email" />
+                        </FormControl>
+                        <PasswordField />
+                    </Stack> */}
+                    {/* <HStack justify="space-between">
+                        <Checkbox defaultChecked>Remember me</Checkbox>
+                        <Button variant="link" colorScheme="blue" size="sm">
+                            Forgot password?
+                        </Button>
+                    </HStack> */}
+                    <Stack spacing="6">
+                        {/* <Button variant="primary">Create Account</Button> */}
+                        <HStack>
+                            <Divider />
+                            <Text fontSize="sm" whiteSpace="nowrap" color="muted">
+                                Sign up with
+                            </Text>
+                            <Divider />
+                        </HStack>
+                        <ButtonGroup variant="outline" spacing="4" width="full">
+                            {providers.map(({ name, icon }) => (
+                                <Button key={name} width="full" onClick={signInWithGoogle}>
+                                    <VisuallyHidden>Sign up with {name}</VisuallyHidden>
+                                    {icon}
+                                </Button>
+                            ))}
+                        </ButtonGroup>
+                    </Stack>
+                </Stack>
+            </Box>
+        </Stack>
+    </Container>);
 }
 
-export default Signup;
+export default SignUp;
