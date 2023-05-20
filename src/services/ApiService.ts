c91cf4caca88091cb46f18561f157ffcbef62f3a
goldvlady <vladyslavabramov@outlook.com>
2023-05-20 17:48:16 +0100

    refactor: organized baseEndpoint url

--- src/services/ApiService.ts
+++ src/services/ApiService.ts
@@ -1,37 +1,38 @@
 import { doFetch } from "../util"
 
 class ApiService {
+    static baseEndpoint = '/.netlify/functions';
 
     static getUser = async () => {
-        return doFetch('/.netlify/functions/me'); 
+        return doFetch(`${ApiService.baseEndpoint}/me`); 
     }
     
     static submitStudentLead = async (data: any) => {
-        return doFetch('/.netlify/functions/createStudentLead', {
+        return doFetch(`${ApiService.baseEndpoint}/createStudentLead`, {
             method: "POST",
             body: JSON.stringify(data)
         })
     }
 
     static submitTutorLead = async (data: any) => {
-        return doFetch('/.netlify/functions/createTutorLead', {
+        return doFetch(`${ApiService.baseEndpoint}/createTutorLead`, {
             method: "POST",
             body: JSON.stringify(data)
         })
     }
 
     static getBookSessionData = async (data: any) => {
-        return doFetch('/.netlify/functions/getBookSessionData?' + new URLSearchParams(data))
+        return doFetch(`${ApiService.baseEndpoint}/getBookSessionData?${new URLSearchParams(data)}`)
     }
 
     static getBooking = async (id: string) => {
-        return doFetch(`/.netlify/functions/getBooking?id=${id}`)
+        return doFetch(`${ApiService.baseEndpoint}/getBooking?id=${id}`)
     }
 
     // Payments
 
     static createBooking = async (data: any) => {
-        return doFetch('/.netlify/functions/createBooking', {
+        return doFetch(`${ApiService.baseEndpoint}/createBooking`, {
             method: "POST",
             body: JSON.stringify(data)
         })
@@ -40,13 +41,13 @@ class ApiService {
     // Tutor
 
     static getTutor = async(id: string) => {
-        return doFetch(`/.netlify/functions/tutor/${id}`);
+        return doFetch(`${ApiService.baseEndpoint}/tutor/${id}`);
     }
 
     // Offer
 
     static createOffer = async (data: any) => {
-        return doFetch('/.netlify/functions/createOffer', {
+        return doFetch(`${ApiService.baseEndpoint}/createOffer`, {
             method: "POST",
             body: JSON.stringify(data)
         })
