a9bb29d2aff69da6052d26e0b04f8fb540cc20c0
goldvlady <vladyslavabramov@outlook.com>
2023-05-21 19:40:51 +0100

    feat: close decline offer  modal on overlay click

--- src/views/Offer.tsx
+++ src/views/Offer.tsx
@@ -177,7 +177,7 @@ const Offer = () => {
                             </ModalFooter>
                         </ModalContent>
                     </Modal>
-                    <Modal closeOnOverlayClick={false} isOpen={isdeclineOfferModalOpen} onClose={onDeclineOfferModalOpen}>
+                    <Modal closeOnOverlayClick={true} isOpen={isdeclineOfferModalOpen} onClose={onDeclineOfferModalOpen}>
                         <ModalOverlay />
                         <ModalContent>
                             <ModalBody padding={0} flexDirection='column'>
