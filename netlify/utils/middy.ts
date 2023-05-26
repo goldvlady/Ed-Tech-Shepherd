c7addd07caebdca72fc1a66e40f5e2b55f7be159
goldvlady <vladyslavabramov@outlook.com>
2023-05-26 20:57:31 +0100

    feat: Added Sentry to API

--- netlify/utils/middy.ts
+++ netlify/utils/middy.ts
@@ -4,21 +4,27 @@ import { connectToDb } from '../database/connection';
 import * as firebaseAdmin from 'firebase-admin';
 import { initializeApp, getApps } from 'firebase-admin/app';
 import serviceAccount from '../serviceAccountKey.json';
+import sentryMiddleware from '../middlewares/sentryMiddleware';
 
 const bootstrapPlugin = () => {
     const requestStart = async () => {
+        // initialize Firebase
         if (!getApps().length) {
             initializeApp({
                 // @ts-ignore
                 credential: firebaseAdmin.credential.cert(serviceAccount)
             })
         }
+
+        // connect to MongoDB
         await connectToDb();
     }
 
     return {
-        requestStart
+        requestStart,
     }
 }
 
-export default (handler: any) => middy(handler, bootstrapPlugin()).use(cors({disableBeforePreflightResponse: false, requestHeaders: '*', requestMethods: '*', headers: '*', methods: '*'}));
+export default (handler: any) => middy(handler, bootstrapPlugin())
+    .use(cors({ disableBeforePreflightResponse: false, requestHeaders: '*', requestMethods: '*', headers: '*', methods: '*' }))
+    .use(sentryMiddleware({ dsn: "https://ac7054d1ea7d4a81adc52cf58774dcab@o4505062795182080.ingest.sentry.io/4505251993354240", }))
