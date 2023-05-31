87b5a1b9135cc9462e6651fa5a9d39feab06e40d
goldvlady <vladyslavabramov@outlook.com>
2023-05-31 21:49:06 +0100

    feat: Refactored getBooking function to match new specs

--- netlify/functions/getBooking.ts
+++ netlify/functions/getBooking.ts
@@ -1,6 +1,5 @@
 import { HandlerEvent } from "@netlify/functions";
-import Booking, { Status } from "../database/models/Booking";
-import BookingHandler from "../handlers/BookingHandler";
+import Booking from "../database/models/Booking";
 import authMiddleware from "../middlewares/authMiddleware";
 import middy from "../utils/middy";
 
@@ -9,7 +8,6 @@ type Params = {
 }
 
 export const getBooking = async (event: HandlerEvent) => {
-    const bookingHandler = new BookingHandler();
     const { id } = event.queryStringParameters as Params;
 
     let booking = await Booking.findById(id);
@@ -17,15 +15,6 @@ export const getBooking = async (event: HandlerEvent) => {
     if (!booking) {
         return {
             statusCode: 404
-        }
-    }
-
-    if (booking.status === Status.UNCONFIRMED) {
-        try {
-            const confirmedBooking = await bookingHandler.confirmBooking(booking);
-            booking = confirmedBooking;
-        } catch (e) {
-            console.log(e);
         }
     }
 
