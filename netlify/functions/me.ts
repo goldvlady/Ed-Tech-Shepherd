e9ad113709632ba29cdacf5fa194698da3aabbdf
goldvlady <vladyslavabramov@outlook.com>
2023-06-05 20:38:01 +0100

    debug: CORS

--- netlify/functions/me.ts
+++ netlify/functions/me.ts
@@ -6,28 +6,10 @@ import { getAuth } from 'firebase-admin/auth';
 import { first, last } from 'lodash';
 
 const me = async (event: HTTPEvent) => {
-    let { firebaseUser, user } = event;
-
-    if (!user) {
-        const firebaseUserObject = await getAuth().getUser(firebaseUser.user_id);
-        const names = firebaseUserObject.displayName?.split(' ');
-
-        user = await User.create({
-            firebaseId: firebaseUser.user_id,
-            email: firebaseUser.email,
-            name: {
-                first: first(names),
-                last: last(names)
-            }
-        })
-    }
-
-    await user.attachLeads();
-
     return {
         statusCode: 200,
-        body: JSON.stringify(user)
+        body: JSON.stringify({'fs': "fd"})
     }
 }
 
-export const handler = middy(me).use(authMiddleware());
+export const handler = middy(me);
