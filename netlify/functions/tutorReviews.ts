a3cbbcda2d84bada47984a8c676ef3c1a48b8480
goldvlady <vladyslavabramov@outlook.com>
2023-06-07 18:29:10 +0000

    Prettified Code!

--- netlify/functions/tutorReviews.ts
+++ netlify/functions/tutorReviews.ts
@@ -5,20 +5,20 @@ import TutorReview from "../database/models/TutorReview";
 import { HTTPEvent } from "../types";
 
 export const tutorReviews = async (event: HTTPEvent) => {
-    const { tutor } = event.queryStringParameters as { tutor: string };
+  const { tutor } = event.queryStringParameters as { tutor: string };
 
-    const reviews = await TutorReview.find({ tutor });
+  const reviews = await TutorReview.find({ tutor });
 
-    if (!tutor) {
-        return {
-            statusCode: 404,
-        };
-    }
-
+  if (!tutor) {
     return {
-        statusCode: 200,
-        body: JSON.stringify(reviews)
+      statusCode: 404,
     };
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(reviews),
+  };
 };
 
 export const handler = middy(tutorReviews);
