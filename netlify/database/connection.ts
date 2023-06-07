744548dd599acfb9011e13c2888c90bb9cfe44f4
goldvlady <vladyslavabramov@outlook.com>
2023-06-07 06:44:07 +0100

    refactor: Code cleanup

--- netlify/database/connection.ts
+++ netlify/database/connection.ts
@@ -2,7 +2,5 @@ import { connect } from "mongoose";
 require("./models/index");
 
 export const connectToDb = async () => {
-  const connection = connect('mongodb+srv://spa:RIRo8R4PMIvn6ilB@cluster0.jukxo1q.mongodb.net/?retryWrites=true&w=majority');
-
-  return connection;
+  return connect('mongodb+srv://spa:RIRo8R4PMIvn6ilB@cluster0.jukxo1q.mongodb.net/?retryWrites=true&w=majority');
 }
