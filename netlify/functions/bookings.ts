2d7213ac44d94a96d36cd7aa75177ee999f7a1db
goldvlady <vladyslavabramov@outlook.com>
2023-06-07 13:30:53 +0100

    feat: /Bookings endpoint

---
+++ netlify/functions/bookings.ts
@@ -0,0 +1,28 @@
+import middy from '../utils/middy';
+import { HTTPEvent } from "../types";
+import authMiddleware from '../middlewares/authMiddleware';
+import Booking from '../database/models/Booking';
+
+const bookings = async (event: HTTPEvent) => {
+    const { user } = event;
+
+    let offerCriteria: Partial<Record<any, {}>> = { 'tutorLead._id': user.tutorLead?._id }
+    if (user.type === 'student') {
+        offerCriteria = { 'studentLead._id': user.studentLead?._id }
+    }
+
+    let allBookings = await Booking.find({ offer: { $ne: null } })
+        .populate({
+            path: "offer",
+            match: {
+                ...offerCriteria
+            }
+        })
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(allBookings)
+    }
+}
+
+export const handler = middy(bookings).use(authMiddleware());
