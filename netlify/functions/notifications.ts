1eae0b189ecf128c0af2d76b215df28fa21db2d0
goldvlady <vladyslavabramov@outlook.com>
2023-06-12 22:49:58 +0100

    feat: Added /notifications endpoint

---
+++ netlify/functions/notifications.ts
@@ -0,0 +1,20 @@
+import UserNotification from '../database/models/UserNotification';
+import authMiddleware from '../middlewares/authMiddleware';
+import { HTTPEvent } from '../types';
+import middy from '../utils/middy';
+
+const notifications = async (event: HTTPEvent) => {
+  let { user } = event;
+  const notifications = UserNotification.find({
+    user: user._id,
+  })
+    .limit(100)
+    .sort({ createdAt: -1 });
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(notifications),
+  };
+};
+
+export const handler = middy(notifications).use(authMiddleware());
