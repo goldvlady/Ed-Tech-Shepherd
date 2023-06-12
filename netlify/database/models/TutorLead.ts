e354cf60172b0c0255ab6018f005f81c07f3af59
goldvlady <vladyslavabramov@outlook.com>
2023-06-12 14:04:16 +0100

    feat: Added tutor.identityDocument

--- netlify/database/models/TutorLead.ts
+++ netlify/database/models/TutorLead.ts
@@ -1,8 +1,9 @@
-import { Schema, model } from "mongoose";
-import { TimestampedEntity } from "../../types";
-import { PipedriveService } from "../../services/PipedriveService";
-import { Course, Schedule } from "../../../src/types";
-import { TutorReview } from "./TutorReview";
+import { Schema, model } from 'mongoose';
+
+import { Course, Schedule } from '../../../src/types';
+import { PipedriveService } from '../../services/PipedriveService';
+import { TimestampedEntity } from '../../types';
+import { TutorReview } from './TutorReview';
 
 export interface TutorLead extends TimestampedEntity {
   name: {
@@ -22,6 +23,7 @@ export interface TutorLead extends TimestampedEntity {
   cv: string;
   teachLevel: string[];
   tz: string;
+  identityDocument?: string;
 
   pipedriveDealId?: string;
 
@@ -45,9 +47,7 @@ const schema = new Schema<TutorLeadSchemaInterface>(
     },
     email: { type: String, required: true },
     dob: { type: String, required: true },
-    courses: [
-      { type: Schema.Types.ObjectId, ref: "Course", autopopulate: true },
-    ],
+    courses: [{ type: Schema.Types.ObjectId, ref: 'Course', autopopulate: true }],
     schedule: { type: Schema.Types.Mixed, required: true },
     rate: { type: Number, required: true },
     active: { type: Boolean, required: false },
@@ -58,6 +58,7 @@ const schema = new Schema<TutorLeadSchemaInterface>(
     cv: { type: String, required: true },
     teachLevel: { type: [String], required: true },
     tz: { type: String, required: true },
+    identityDocument: { type: String, required: false },
 
     pipedriveDealId: { type: String, required: false },
   },
@@ -79,7 +80,7 @@ const schema = new Schema<TutorLeadSchemaInterface>(
 );
 
 // @ts-expect-error
-schema.post(["update", "findOneAndUpdate", "updateOne"], async function () {
+schema.post(['update', 'findOneAndUpdate', 'updateOne'], async function () {
   // @ts-expect-error
   const docToUpdate = await this.model.findOne(this.getQuery());
 
@@ -91,27 +92,22 @@ schema.post(["update", "findOneAndUpdate", "updateOne"], async function () {
   await pd.updateTutorDeal(docToUpdate);
 });
 
-schema.virtual("reviews", {
-  ref: "TutorReview",
-  localField: "_id",
-  foreignField: "tutor",
+schema.virtual('reviews', {
+  ref: 'TutorReview',
+  localField: '_id',
+  foreignField: 'tutor',
   autopopulate: true,
 });
 
-schema.virtual("reviewCount").get(function () {
+schema.virtual('reviewCount').get(function () {
   return this.reviews.length;
 });
 
-schema.virtual("rating").get(function () {
-  const ratingSum: number = this.reviews.reduce(
-    (total, a) => total + a.rating,
-    0
-  );
-  return ratingSum !== 0
-    ? parseFloat((ratingSum / this.reviews.length).toFixed(2))
-    : ratingSum;
+schema.virtual('rating').get(function () {
+  const ratingSum: number = this.reviews.reduce((total, a) => total + a.rating, 0);
+  return ratingSum !== 0 ? parseFloat((ratingSum / this.reviews.length).toFixed(2)) : ratingSum;
 });
 
-schema.plugin(require("mongoose-autopopulate"));
+schema.plugin(require('mongoose-autopopulate'));
 
-export default model<TutorLead>("TutorLead", schema);
+export default model<TutorLead>('TutorLead', schema);
