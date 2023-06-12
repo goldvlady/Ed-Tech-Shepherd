8a8933d5974e0c93f660a23b69fb1e9c652eff82
goldvlady <vladyslavabramov@outlook.com>
2023-06-12 22:44:40 +0100

    feat: Added UserNotification entity

---
+++ netlify/database/models/UserNotification.ts
@@ -0,0 +1,25 @@
+import { Schema, model } from 'mongoose';
+
+import { TimestampedEntity } from '../../types';
+import { User } from './User';
+
+export enum Types {
+  LESSON_SESSION_STARTED = 'lesson_session_started',
+}
+
+export interface UserNotification extends TimestampedEntity {
+  user: User;
+  text: string;
+  type: Types;
+}
+
+const schema = new Schema<UserNotification>(
+  {
+    user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    text: { type: String, required: true },
+    type: { type: String, enum: Types, required: true },
+  },
+  { timestamps: { createdAt: true, updatedAt: false } }
+);
+
+export default model<UserNotification>('UserNotification', schema);
