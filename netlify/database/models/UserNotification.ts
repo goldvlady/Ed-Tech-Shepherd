ca68fb5f42a8d91f0552b71773da64f7c9a4e1b1
goldvlady <vladyslavabramov@outlook.com>
2023-06-13 11:52:10 +0100

    feat: Added attributes to UserNotification

--- netlify/database/models/UserNotification.ts
+++ netlify/database/models/UserNotification.ts
@@ -11,6 +11,7 @@ export interface UserNotification extends TimestampedEntity {
   user: User;
   text: string;
   type: Types;
+  attributes?: Record<any, any>;
   readAt?: Date;
 }
 
@@ -19,6 +20,7 @@ const schema = new Schema<UserNotification>(
     user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
     text: { type: String, required: true },
     type: { type: String, enum: Types, required: true },
+    attributes: { type: Schema.Types.Mixed, required: false },
     readAt: { type: Date, required: false },
   },
   { timestamps: { createdAt: true, updatedAt: false } }
