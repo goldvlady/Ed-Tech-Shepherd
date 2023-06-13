1e49bc5372ab6dfda31c4d3914eb4db65f0945fd
goldvlady <vladyslavabramov@outlook.com>
2023-06-13 13:45:07 +0100

    feat: Added tutorLead to User

--- netlify/database/models/User.ts
+++ netlify/database/models/User.ts
@@ -1,8 +1,9 @@
-import { Schema, model } from "mongoose";
-import { TimestampedEntity } from "../../types";
-import StudentLead, { StudentLead as StudentLeadType } from "./StudentLead";
-import TutorLead, { TutorLead as TutorLeadType } from "./TutorLead";
-import { PaymentMethod } from "./PaymentMethod";
+import { Schema, model } from 'mongoose';
+
+import { TimestampedEntity } from '../../types';
+import { PaymentMethod } from './PaymentMethod';
+import StudentLead, { StudentLead as StudentLeadType } from './StudentLead';
+import TutorLead, { TutorLead as TutorLeadType } from './TutorLead';
 
 export interface User extends TimestampedEntity {
   name: {
@@ -15,7 +16,7 @@ export interface User extends TimestampedEntity {
   tutorLead?: TutorLeadType;
   studentLead?: StudentLeadType;
   attachLeads: () => Promise<User>;
-  type: "student" | "tutor";
+  type: 'student' | 'tutor';
   stripeCustomerId?: string;
   paymentMethods: PaymentMethod[];
 }
@@ -36,36 +37,71 @@ const schema = new Schema<User>(
   },
   {
     timestamps: true,
-    toJSON: { virtuals: true },
-    toObject: { virtuals: true },
+    toJSON: {
+      virtuals: true,
+      transform: (doc, r) => {
+        if (r.tutorLead) {
+          delete r.tutorLead.user;
+        }
+      },
+    },
+    toObject: {
+      virtuals: true,
+      transform: (doc, r) => {
+        if (r.tutorLead) {
+          delete r.tutorLead.user;
+        }
+      },
+    },
   }
 );
 
-schema.virtual("tutorLead");
-schema.virtual("studentLead");
-schema.virtual("type");
+schema.virtual('tutorLead');
+schema.virtual('studentLead');
+schema.virtual('type');
 
-schema.virtual("paymentMethods", {
-  ref: "PaymentMethod",
-  localField: "_id",
-  foreignField: "user",
+schema.virtual('paymentMethods', {
+  ref: 'PaymentMethod',
+  localField: '_id',
+  foreignField: 'user',
+});
+
+schema.virtual('tutorLead', {
+  ref: 'TutorLead',
+  localField: 'email',
+  foreignField: 'email',
+  justOne: true,
+  autopopulate: true,
+  options: {
+    match: {
+      active: true,
+    },
+  },
+});
+
+schema.virtual('type').get(function () {
+  if (this.tutorLead) {
+    return 'tutor';
+  }
+
+  return 'student';
 });
 
 schema.methods.attachLeads = async function (cb: any) {
-  const tutorLead = await TutorLead.findOne({ email: this.email });
-  this.tutorLead = tutorLead;
+  // const tutorLead = await TutorLead.findOne({ email: this.email });
+  // this.tutorLead = tutorLead;
 
-  const studentLead = await StudentLead.findOne({ email: this.email });
-  this.studentLead = studentLead;
+  // const studentLead = await StudentLead.findOne({ email: this.email });
+  // this.studentLead = studentLead;
 
-  this.type = "student";
-  if (this.tutorLead) {
-    this.type = "tutor";
-  }
+  // this.type = "student";
+  // if (this.tutorLead) {
+  //   this.type = "tutor";
+  // }
 
   return this;
 };
 
-schema.plugin(require("mongoose-autopopulate"));
+schema.plugin(require('mongoose-autopopulate'));
 
-export default model<User>("User", schema);
+export default model<User>('User', schema);
