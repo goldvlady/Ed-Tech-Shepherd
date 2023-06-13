a87f93e94788580b68545d356f669d4777717b6d
goldvlady <vladyslavabramov@outlook.com>
2023-06-13 13:56:56 +0100

    feat: Added NotificationHandler class

---
+++ netlify/handlers/NotificationHandler.ts
@@ -0,0 +1,23 @@
+import { Offer } from '../database/models/Offer';
+import UserNotificationModel, {
+  Types,
+  UserNotification,
+} from '../database/models/UserNotification';
+import { TimestampedEntity } from '../types';
+
+class NotificationHandler {
+  public async createNotification(params: Omit<UserNotification, keyof TimestampedEntity>) {
+    return await UserNotificationModel.create(params);
+  }
+
+  public async createNewOfferReceivedNotification(offer: Offer) {
+    await this.createNotification({
+      text: 'You received a new contract offer',
+      type: Types.NEW_OFFER_RECEIVED,
+      user: offer.tutorLead.user,
+      attributes: { offerId: offer._id },
+    });
+  }
+}
+
+export default NotificationHandler;
