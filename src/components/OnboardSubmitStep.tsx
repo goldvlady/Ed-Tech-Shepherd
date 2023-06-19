7bb9c98e4ccea0fcca2fc6d5f1095016e51f53c9
goldvlady <vladyslavabramov@outlook.com>
2023-06-19 16:45:50 +0100

    complete profile integration

--- src/components/OnboardSubmitStep.tsx
+++ src/components/OnboardSubmitStep.tsx
@@ -1,38 +1,40 @@
-import { Box } from '@chakra-ui/react';
-import * as React from 'react';
-import { useEffect } from 'react';
+import { Box } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
+import * as React from "react";
+import { useEffect } from "react";
 
-import OnboardNav from './OnboardNav';
+import OnboardNav from "./OnboardNav";
 
 type Props = {
-    children: React.ReactNode;
-    submitFunction: () => Promise<Response>;
-} & Omit<React.ComponentPropsWithoutRef<typeof OnboardNav>, 'canGoNext'>;
+  children: React.ReactNode;
+  submitFunction: () => Promise<Response>;
+} & Omit<React.ComponentPropsWithoutRef<typeof OnboardNav>, "canGoNext">;
 
 const OnboardSubmitStep: React.FC<Props> = ({
-    children,
-    isActive,
-    submitFunction,
-    nextStep,
-    previousStep,
-    currentStep,
+  children,
+  isActive,
+  submitFunction,
+  nextStep,
+  previousStep,
+  currentStep,
 }) => {
-    useEffect(() => {
-        if (isActive) {
-            submit();
-        }
-    }, [isActive]);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (isActive) {
+      submit();
+    }
+  }, [isActive]);
 
-    const submit = async () => {
-        try {
-            await submitFunction();
-            nextStep?.();
-        } catch (e) {
-            previousStep?.();
-        }
-    };
+  const submit = async () => {
+    try {
+      await submitFunction();
+      navigate("/verification_pending");
+    } catch (e) {
+      previousStep?.();
+    }
+  };
 
-    return <Box>{children}</Box>;
+  return <Box>{children}</Box>;
 };
 
 export default OnboardSubmitStep;
