d95473c7d3ad0a3e56aa0a4cce8ea04f1c2478d9
goldvlady <vladyslavabramov@outlook.com>
2023-06-26 19:10:14 +0100

    Corections and fixes

--- src/components/SelectedNoteModal.tsx
+++ src/components/SelectedNoteModal.tsx
@@ -110,7 +110,7 @@ const SelectedModal: React.FC<selectedModalProps> = ({
                   <button
                     type="button"
                     className="inline-flex w-fit justify-center rounded-md bg-primaryBlue px-3 py-2 text-sm font-semibold text-white shadow-md hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
-                    onClick={() => navigate(`/docchat`)}
+                    onClick={() => navigate(`/dashboard/docchat`)}
                   >
                     Confirm
                   </button>
