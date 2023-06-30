ffc4833bf973c5a8146f5e236b81687b3502ceff
goldvlady <vladyslavabramov@outlook.com>
2023-06-30 13:29:34 +0100

    refactor docchat file

---
+++ src/views/docchat/index.tsx
@@ -0,0 +1,28 @@
+import { PdfViewer } from "../../components";
+import Chat from "./chat";
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+
+// @ts-ignore
+export default function DocChat() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!location.state?.documentUrl) navigate("/dashboard/notes");
+  }, [navigate, location.state?.documentUrl]);
+
+  return (
+    location.state?.documentUrl && (
+      <section className="divide-y max-w-screen-xl mx-auto">
+        <div className="h-screen divide-y divide-gray-200 lg:grid lg:grid-cols-12 lg:divide-y-0 lg:divide-x">
+          <PdfViewer
+            documentUrl={location.state.documentUrl}
+            docTitle={location.state.docTitle}
+          />
+          <Chat />
+        </div>
+      </section>
+    )
+  );
+}
