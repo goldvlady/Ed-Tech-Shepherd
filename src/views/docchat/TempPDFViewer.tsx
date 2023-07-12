8e56ac9255e2ca5a5813fb054e3149cf9f9c2498
goldvlady <vladyslavabramov@outlook.com>
2023-07-12 12:54:59 +0100

    fix dependency conflicts

--- src/views/docchat/TempPDFViewer.tsx
+++ src/views/docchat/TempPDFViewer.tsx
@@ -1,6 +1,26 @@
-const TempPDFViewer = ({url}: { url: any}) => {
+import React, { Component } from 'react';
+import { PdfLoader, PdfHighlighter, Tip, Highlight, AreaHighlight, Popup } from 'react-pdf-highlighter';
+import type { IHighlight, NewHighlight } from "react-pdf-highlighter";
+
+const HighlightPopup = ({
+  comment,
+}: {
+  comment: { text: string; emoji: string };
+}) =>
+  comment.text ? (
+    <div className="Highlight__popup">
+      {comment.emoji} {comment.text}
+    </div>
+  ) : null;
+
+
+const TempPDFViewer = ({ url }: { url: any }) => {
     return (
-        <div>Heyyy</div>
+        <div>
+            <PdfLoader url={url} beforeLoad={<div></div>}>
+                {(pdfDocument) => <div></div>}
+        </PdfLoader>
+            </div>
     )
 }
 
