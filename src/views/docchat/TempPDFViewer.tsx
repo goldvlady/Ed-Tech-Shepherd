6d20b35059b90f99717f95090faa3f1027c94c1a
goldvlady <vladyslavabramov@outlook.com>
2023-07-12 14:06:21 +0100

    shameful patches for a very time-consuming Typescript issue

--- src/views/docchat/TempPDFViewer.tsx
+++ src/views/docchat/TempPDFViewer.tsx
@@ -1,16 +1,40 @@
-import React, { Component } from 'react';
+import { Component } from "react";
+
 import {
   PdfLoader,
   PdfHighlighter,
   Tip,
   Highlight,
+  Popup,
   AreaHighlight,
-  Popup
-} from 'react-pdf-highlighter';
-import type { IHighlight, NewHighlight } from 'react-pdf-highlighter';
+} from "react-pdf-highlighter";
+
+import type { IHighlight, NewHighlight } from "react-pdf-highlighter";
+
+import { testHighlights as _testHighlights } from "./test-highlights";
+import { Spinner } from "./Spinner";
+import { Sidebar } from "./Sidebar";
+
+import "./style/App.css";
+
+const testHighlights: Record<string, Array<IHighlight>> = _testHighlights;
+
+interface State {
+  url: string;
+  highlights: Array<IHighlight>;
+}
+
+const getNextId = () => String(Math.random()).slice(2);
+
+const parseIdFromHash = () =>
+  document.location.hash.slice("#highlight-".length);
+
+const resetHash = () => {
+  document.location.hash = "";
+};
 
 const HighlightPopup = ({
-  comment
+  comment,
 }: {
   comment: { text: string; emoji: string };
 }) =>
@@ -20,14 +44,197 @@ const HighlightPopup = ({
     </div>
   ) : null;
 
-const TempPDFViewer = ({ url }: { url: any }) => {
-  return (
-    <div>
-      <PdfLoader url={url} beforeLoad={<div></div>}>
-        {(pdfDocument) => <div></div>}
-      </PdfLoader>
-    </div>
-  );
-};
+const PRIMARY_PDF_URL = "https://arxiv.org/pdf/1708.08021.pdf";
+const SECONDARY_PDF_URL = "https://arxiv.org/pdf/1604.02480.pdf";
+
+const searchParams = new URLSearchParams(document.location.search);
+
+const initialUrl = searchParams.get("url") || PRIMARY_PDF_URL;
+
+class TempPDFViewer extends Component<object, State> {
+  state = {
+    url: initialUrl,
+    highlights: testHighlights[initialUrl]
+      ? [...testHighlights[initialUrl]]
+      : [],
+  };
+
+  resetHighlights = () => {
+    this.setState({
+      highlights: [],
+    });
+  };
+
+  toggleDocument = () => {
+    const newUrl =
+      this.state.url === PRIMARY_PDF_URL ? SECONDARY_PDF_URL : PRIMARY_PDF_URL;
+
+    this.setState({
+      url: newUrl,
+      highlights: testHighlights[newUrl] ? [...testHighlights[newUrl]] : [],
+    });
+  };
+
+    scrollViewerTo = (highlight: any) => {
+      // we will fill this in later;
+  };
+
+  scrollToHighlightFromHash = () => {
+    const highlight = this.getHighlightById(parseIdFromHash());
+
+    if (highlight) {
+      this.scrollViewerTo(highlight);
+    }
+  };
+
+  componentDidMount() {
+    window.addEventListener(
+      "hashchange",
+      this.scrollToHighlightFromHash,
+      false
+    );
+  }
+
+  getHighlightById(id: string) {
+    const { highlights } = this.state;
+
+    return highlights.find((highlight) => highlight.id === id);
+  }
+
+  addHighlight(highlight: NewHighlight) {
+    const { highlights } = this.state;
+
+    console.log("Saving highlight", highlight);
+
+    this.setState({
+      highlights: [{ ...highlight, id: getNextId() }, ...highlights],
+    });
+  }
+
+  updateHighlight(highlightId: string, position: object, content: object) {
+    console.log("Updating highlight", highlightId, position, content);
+
+    this.setState({
+      highlights: this.state.highlights.map((h) => {
+        const {
+          id,
+          position: originalPosition,
+          content: originalContent,
+          ...rest
+        } = h;
+        return id === highlightId
+          ? {
+              id,
+              position: { ...originalPosition, ...position },
+              content: { ...originalContent, ...content },
+              ...rest,
+            }
+          : h;
+      }),
+    });
+  }
+
+  render() {
+    const { url, highlights } = this.state;
+
+    return (
+      <div className="App" style={{ display: "flex", height: "100vh" }}>
+        <Sidebar
+          highlights={highlights}
+          resetHighlights={this.resetHighlights}
+          toggleDocument={this.toggleDocument}
+        />
+        <div
+          style={{
+            height: "100vh",
+            width: "75vw",
+            position: "relative",
+          }}
+            >
+                {/* @ts-ignore: this is a documented error regarding TS2786. I don't know how to fix yet (ref: https://stackoverflow.com/questions/72002300/ts2786-typescript-not-reconizing-ui-kitten-components)  */}
+            <PdfLoader url={url} beforeLoad={<Spinner />}>
+            {(pdfDocument) => (        
+            // @ts-ignore: same issue as linked above
+              <PdfHighlighter
+                pdfDocument={pdfDocument}
+                enableAreaSelection={(event) => event.altKey}
+                onScrollChange={resetHash}
+                // pdfScaleValue="page-width"
+                scrollRef={(scrollTo) => {
+                  this.scrollViewerTo = scrollTo;
+
+                  this.scrollToHighlightFromHash();
+                }}
+                onSelectionFinished={(
+                  position,
+                  content,
+                  hideTipAndSelection,
+                  transformSelection
+                ) => (
+                // @ts-ignore: same issue as linked above
+                  <Tip
+                    onOpen={transformSelection}
+                    onConfirm={(comment) => {
+                      this.addHighlight({ content, position, comment });
+
+                      hideTipAndSelection();
+                    }}
+                  />
+                )}
+                highlightTransform={(
+                  highlight,
+                  index,
+                  setTip,
+                  hideTip,
+                  viewportToScaled,
+                  screenshot,
+                  isScrolledTo
+                ) => {
+                  const isTextHighlight = !(highlight.content && highlight.content.image);
+
+                  const component = isTextHighlight ? (
+                    // @ts-ignore: same issue as linked above
+                    <Highlight
+                      isScrolledTo={isScrolledTo}
+                      position={highlight.position}
+                      comment={highlight.comment}
+                    />
+                  ) : (
+                    // @ts-ignore: same issue as linked above
+                    <AreaHighlight
+                      isScrolledTo={isScrolledTo}
+                      highlight={highlight}
+                      onChange={(boundingRect) => {
+                        this.updateHighlight(
+                          highlight.id,
+                          { boundingRect: viewportToScaled(boundingRect) },
+                          { image: screenshot(boundingRect) }
+                        );
+                      }}
+                    />
+                  );
+
+                  return (
+                    // @ts-ignore: same issue as linked above
+                    <Popup
+                      popupContent={<HighlightPopup {...highlight} />}
+                      onMouseOver={(popupContent) =>
+                        setTip(highlight, (highlight) => popupContent)
+                      }
+                      onMouseOut={hideTip}
+                      key={index}
+                      children={component}
+                    />
+                  );
+                }}
+                highlights={highlights}
+              />
+            )}
+          </PdfLoader>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default TempPDFViewer;
