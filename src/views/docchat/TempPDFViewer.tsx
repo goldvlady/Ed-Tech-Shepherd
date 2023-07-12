736b2caef9e78cc010f884f8a97d980dc8de6b73
goldvlady <vladyslavabramov@outlook.com>
2023-07-12 15:25:58 +0100

    thanks prettier

--- src/views/docchat/TempPDFViewer.tsx
+++ src/views/docchat/TempPDFViewer.tsx
@@ -3,7 +3,14 @@ import { Spinner } from './Spinner';
 import { testHighlights as _testHighlights } from './test-highlights';
 import { useEffect, useState } from 'react';
 import type { IHighlight, NewHighlight } from 'react-pdf-highlighter';
-import { PdfLoader, PdfHighlighter, Tip, Highlight, Popup, AreaHighlight } from 'react-pdf-highlighter';
+import {
+  PdfLoader,
+  PdfHighlighter,
+  Tip,
+  Highlight,
+  Popup,
+  AreaHighlight
+} from 'react-pdf-highlighter';
 
 const testHighlights: Record<string, Array<IHighlight>> = _testHighlights;
 
@@ -27,10 +34,9 @@ const HighlightPopup = ({
     </div>
   ) : null;
 
-
 const TempPDFViewer = ({ pdfLink }: { pdfLink: URL }) => {
-    const [highlights, setHighlights] = useState<Array<IHighlight>>([]);
-    const [url, setUrl] = useState(pdfLink)
+  const [highlights, setHighlights] = useState<Array<IHighlight>>([]);
+  const [url, setUrl] = useState(pdfLink);
   const resetHighlights = () => {
     setHighlights([]);
   };
@@ -47,138 +53,147 @@ const TempPDFViewer = ({ pdfLink }: { pdfLink: URL }) => {
     }
   };
 
-    useEffect(() => {
-        if (window) window.addEventListener('hashchange', scrollToHighlightFromHash, false);
-    }, [])
+  useEffect(() => {
+    if (window)
+      window.addEventListener('hashchange', scrollToHighlightFromHash, false);
+  }, []);
 
   const getHighlightById = (id: string) => {
     return highlights.find((highlight) => highlight.id === id);
-  }
+  };
 
   const addHighlight = (highlight: NewHighlight) => {
-      setHighlights([{ ...highlight, id: getNextId() }, ...highlights]);
-  }
+    setHighlights([{ ...highlight, id: getNextId() }, ...highlights]);
+  };
 
-  const updateHighlight = (highlightId: string, position: object, content: object) => {
-      const updated = highlights.map(highlight => {
-          const { id, position: originalPosition, content: originalContent, ...rest } = highlight;
+  const updateHighlight = (
+    highlightId: string,
+    position: object,
+    content: object
+  ) => {
+    const updated = highlights.map((highlight) => {
+      const {
+        id,
+        position: originalPosition,
+        content: originalContent,
+        ...rest
+      } = highlight;
 
-          if (id === highlightId) {
-              return {
-                  id,
-                  position: { ...originalPosition, ...position },
-                  content: { ...originalContent, ...content },
-                  ...rest
-              }
-          } else return highlight
-      });
+      if (id === highlightId) {
+        return {
+          id,
+          position: { ...originalPosition, ...position },
+          content: { ...originalContent, ...content },
+          ...rest
+        };
+      } else return highlight;
+    });
 
-      setHighlights(updated);
+    setHighlights(updated);
+  };
 
-  }
-
-    return (
-      <div
-        style={{ display: 'flex', height: '100vh', width: '100%' }}
-        className="lg:col-span-6 flex-auto h-full"
-      >
-        {/* <Sidebar
+  return (
+    <div
+      style={{ display: 'flex', height: '100vh', width: '100%' }}
+      className="lg:col-span-6 flex-auto h-full"
+    >
+      {/* <Sidebar
           highlights={highlights}
           resetHighlights={this.resetHighlights}
           toggleDocument={this.toggleDocument}
         /> */}
-        <div
-          style={{
-            height: '100vh',
-            width: '75vw',
-            position: 'relative'
-          }}
-        >
-          {/* @ts-ignore: this is a documented error regarding TS2786. I don't know how to fix yet (ref: https://stackoverflow.com/questions/72002300/ts2786-typescript-not-reconizing-ui-kitten-components)  */}
-          <PdfLoader url={url} beforeLoad={<Spinner />}>
-            {(pdfDocument) => (
-              // @ts-ignore: same issue as linked above
-              <PdfHighlighter
-                pdfDocument={pdfDocument}
-                enableAreaSelection={(event) => event.altKey}
-                onScrollChange={resetHash}
-                // pdfScaleValue="page-width"
-                scrollRef={(scrollTo) => {
-                  scrollViewerTo = scrollTo;
+      <div
+        style={{
+          height: '100vh',
+          width: '75vw',
+          position: 'relative'
+        }}
+      >
+        {/* @ts-ignore: this is a documented error regarding TS2786. I don't know how to fix yet (ref: https://stackoverflow.com/questions/72002300/ts2786-typescript-not-reconizing-ui-kitten-components)  */}
+        <PdfLoader url={url} beforeLoad={<Spinner />}>
+          {(pdfDocument) => (
+            // @ts-ignore: same issue as linked above
+            <PdfHighlighter
+              pdfDocument={pdfDocument}
+              enableAreaSelection={(event) => event.altKey}
+              onScrollChange={resetHash}
+              // pdfScaleValue="page-width"
+              scrollRef={(scrollTo) => {
+                scrollViewerTo = scrollTo;
 
-                  scrollToHighlightFromHash();
-                }}
-                onSelectionFinished={(
-                  position,
-                  content,
-                  hideTipAndSelection,
-                  transformSelection
-                ) => (
+                scrollToHighlightFromHash();
+              }}
+              onSelectionFinished={(
+                position,
+                content,
+                hideTipAndSelection,
+                transformSelection
+              ) => (
+                // @ts-ignore: same issue as linked above
+                <Tip
+                  onOpen={transformSelection}
+                  onConfirm={(comment) => {
+                    addHighlight({ content, position, comment });
+
+                    hideTipAndSelection();
+                  }}
+                />
+              )}
+              highlightTransform={(
+                highlight,
+                index,
+                setTip,
+                hideTip,
+                viewportToScaled,
+                screenshot,
+                isScrolledTo
+              ) => {
+                const isTextHighlight = !(
+                  highlight.content && highlight.content.image
+                );
+
+                const component = isTextHighlight ? (
                   // @ts-ignore: same issue as linked above
-                  <Tip
-                    onOpen={transformSelection}
-                    onConfirm={(comment) => {
-                      addHighlight({ content, position, comment });
-
-                      hideTipAndSelection();
+                  <Highlight
+                    isScrolledTo={isScrolledTo}
+                    position={highlight.position}
+                    comment={highlight.comment}
+                  />
+                ) : (
+                  // @ts-ignore: same issue as linked above
+                  <AreaHighlight
+                    isScrolledTo={isScrolledTo}
+                    highlight={highlight}
+                    onChange={(boundingRect) => {
+                      updateHighlight(
+                        highlight.id,
+                        { boundingRect: viewportToScaled(boundingRect) },
+                        { image: screenshot(boundingRect) }
+                      );
                     }}
                   />
-                )}
-                highlightTransform={(
-                  highlight,
-                  index,
-                  setTip,
-                  hideTip,
-                  viewportToScaled,
-                  screenshot,
-                  isScrolledTo
-                ) => {
-                  const isTextHighlight = !(
-                    highlight.content && highlight.content.image
-                  );
+                );
 
-                  const component = isTextHighlight ? (
-                    // @ts-ignore: same issue as linked above
-                    <Highlight
-                      isScrolledTo={isScrolledTo}
-                      position={highlight.position}
-                      comment={highlight.comment}
-                    />
-                  ) : (
-                    // @ts-ignore: same issue as linked above
-                    <AreaHighlight
-                      isScrolledTo={isScrolledTo}
-                      highlight={highlight}
-                      onChange={(boundingRect) => {
-                        updateHighlight(
-                          highlight.id,
-                          { boundingRect: viewportToScaled(boundingRect) },
-                          { image: screenshot(boundingRect) }
-                        );
-                      }}
-                    />
-                  );
-
-                  return (
-                    // @ts-ignore: same issue as linked above
-                    <Popup
-                      popupContent={<HighlightPopup {...highlight} />}
-                      onMouseOver={(popupContent) =>
-                        setTip(highlight, (highlight) => popupContent)
-                      }
-                      onMouseOut={hideTip}
-                      key={index}
-                      children={component}
-                    />
-                  );
-                }}
-                highlights={highlights}
-              />
-            )}
-          </PdfLoader>
-        </div>
+                return (
+                  // @ts-ignore: same issue as linked above
+                  <Popup
+                    popupContent={<HighlightPopup {...highlight} />}
+                    onMouseOver={(popupContent) =>
+                      setTip(highlight, (highlight) => popupContent)
+                    }
+                    onMouseOut={hideTip}
+                    key={index}
+                    children={component}
+                  />
+                );
+              }}
+              highlights={highlights}
+            />
+          )}
+        </PdfLoader>
       </div>
-    );
-}
+    </div>
+  );
+};
 
 export default TempPDFViewer;
