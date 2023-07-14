57e57331c450ca58e87e1edf481c96a938c860c6
goldvlady <vladyslavabramov@outlook.com>
2023-07-14 16:38:10 +0100

    implement flashcard fixes

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,2 +1,42 @@
+import { debounce } from 'lodash';
+import { useState, useEffect, useCallback } from 'react';
+
+type SearchAction = (query: string) => void;
+
 export const useTitle = (title: string) =>
-    (document.title = title ? `${title} - Shepherd Tutors` : 'Shepherd Tutors');
+  (document.title = title ? `${title} - Shepherd Tutors` : 'Shepherd Tutors');
+
+export const useSearch = (
+  action: SearchAction,
+  throttleTime = 1000
+): SearchAction => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [hasSearched, setHasSearched] = useState(false);
+  const throttledAction = useCallback(debounce(action, throttleTime), [
+    action,
+    throttleTime
+  ]);
+
+  useEffect(() => {
+    if (searchQuery) {
+      throttledAction(searchQuery);
+    }
+  }, [searchQuery, throttledAction, hasSearched]);
+
+  const handleSearch: SearchAction = useCallback(
+    (query) => {
+      if (query) {
+        setSearchQuery(query);
+      }
+      if (!query && hasSearched) {
+        action(query);
+      }
+      if (!hasSearched) {
+        setHasSearched(true);
+      }
+    },
+    [hasSearched, action]
+  );
+
+  return handleSearch;
+};
