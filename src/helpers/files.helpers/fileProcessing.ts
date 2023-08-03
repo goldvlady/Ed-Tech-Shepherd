76cde835fcab2319a05e029d0f5730f0da09765d
goldvlady <vladyslavabramov@outlook.com>
2023-08-03 23:29:50 +0100

    long-polling for status

--- src/helpers/files.helpers/fileProcessing.ts
+++ src/helpers/files.helpers/fileProcessing.ts
@@ -30,39 +30,27 @@ class FileProcessingService {
     throw new Error('Failed to save document');
   }
 
-  private checkIngested(studentId: string, documentId: string) {
-    let attempts = 0;
+  async checkIngested(studentId: string, documentId: string) {
+    let status;
 
-    const checkStatus = async (
-      resolve: (value: string) => void,
-      reject: (reason?: any) => void
-    ) => {
-      try {
-        if (attempts > 10) {
-          reject('Document processing failed');
-          return;
-        }
-
-        const check = await this.apiService.checkDocumentStatus({
+    const subscribe = async () => {
+      const response = await this.apiService
+        .checkDocumentStatus({
           studentId,
           documentId
-        });
-        if (check.status === 'ingested') {
-          resolve('Document successfully ingested');
-        } else if (check.status === 'too_large') {
-          reject('Document is too large');
-        } else if (check.status === 'failed') {
-          reject('Failed to process file');
-        } else {
-          attempts++;
-          setTimeout(checkStatus, 10000, resolve, reject);
-        }
-      } catch (error) {
-        reject(error);
+        })
+        .then((res) => res.json());
+
+      if (response.pollStatus === 'done') {
+        status = response.documentStatus;
+      } else {
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+        await subscribe();
       }
     };
 
-    return new Promise<string>(checkStatus);
+    await subscribe();
+    return status;
   }
 }
 
