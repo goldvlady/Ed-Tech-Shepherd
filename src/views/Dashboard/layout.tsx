84655f71c5f27757babe540009cdc8c8832465ac
goldvlady <vladyslavabramov@outlook.com>
2023-08-12 23:59:13 +0100

    Recent transactions for Tutor feeds

--- src/views/Dashboard/layout.tsx
+++ src/views/Dashboard/layout.tsx
@@ -101,12 +101,12 @@ const LinkItems: Array<LinkItemProps> = [
 ];
 
 const LinkBItems: Array<LinkItemProps> = [
-  { name: 'Performance', icon: FiBarChart2, path: '/dashboard/performance' },
-  {
-    name: 'Study Plans',
-    icon: TbClipboardText,
-    path: '/dashboard/study-plans'
-  },
+  // { name: 'Performance', icon: FiBarChart2, path: '/dashboard/performance' },
+  // {
+  //   name: 'Study Plans',
+  //   icon: TbClipboardText,
+  //   path: '/dashboard/study-plans'
+  // },
   { name: 'Notes', icon: CgNotes, path: '/dashboard/notes' },
   { name: 'Flashcards', icon: TbCards, path: '/dashboard/flashcards' }
 ];
@@ -285,7 +285,12 @@ const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
                     icon={<FaBell />}
                   />{' '}
                 </MenuButton>
-                <MenuList p={3} width={'358px'} zIndex={2}>
+                <MenuList
+                  p={3}
+                  width={'358px'}
+                  zIndex={2}
+                  sx={{ position: 'absolute', top: '50px' }}
+                >
                   <Notifications data={userNotifications} />
                 </MenuList>
               </Menu>
