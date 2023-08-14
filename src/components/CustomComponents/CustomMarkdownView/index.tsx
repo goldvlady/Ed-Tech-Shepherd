e8286f1e877538bae343ce7d46b1511c993a2a53
goldvlady <vladyslavabramov@outlook.com>
2023-08-14 03:10:07 -0600

    latex and code styling

--- src/components/CustomComponents/CustomMarkdownView/index.tsx
+++ src/components/CustomComponents/CustomMarkdownView/index.tsx
@@ -1,18 +1,42 @@
 import './index.css';
-import React, { useState, useEffect, useRef } from 'react';
+import 'katex/dist/katex.min.css';
+import React from 'react';
+import { BlockMath, InlineMath } from 'react-katex';
 import ReactMarkdown from 'react-markdown';
 import rehypePrism from 'rehype-prism';
 import rehypeRaw from 'rehype-raw';
 import remarkGfm from 'remark-gfm';
+import remarkMath from 'remark-math';
 
-const CustomMarkdownView: React.FC<{ source: string; delay?: number }> = ({
-  source
-}) => {
+interface CustomMarkdownViewProps {
+  source: string;
+}
+
+interface CustomComponents {
+  math: ({ node, inline }: { node: any; inline: boolean }) => JSX.Element;
+  inlineMath: ({ node, inline }: { node: any; inline: boolean }) => JSX.Element;
+  // Add more custom components if needed
+}
+
+const CustomMarkdownView: React.FC<CustomMarkdownViewProps> = ({ source }) => {
+  const components: CustomComponents = {
+    math: ({ node, inline }) => {
+      if (inline) {
+        return <InlineMath math={node.value} />;
+      }
+      return <BlockMath math={node.value} />;
+    },
+    inlineMath: ({ node, inline }) => {
+      return <InlineMath math={node.value} />;
+    }
+  };
+
   return (
     <ReactMarkdown
       className="custom_markdown"
-      remarkPlugins={[remarkGfm]}
+      remarkPlugins={[remarkGfm, remarkMath]}
       rehypePlugins={[rehypeRaw, rehypePrism]}
+      components={components as any}
       children={source}
     />
   );
