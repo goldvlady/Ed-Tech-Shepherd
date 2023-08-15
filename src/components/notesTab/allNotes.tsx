39960bef9f7567e51ff88bc7e106d40dcfdb8424
goldvlady <vladyslavabramov@outlook.com>
2023-08-15 22:03:13 +0100

    refactor: fix tag view issue

--- src/components/notesTab/allNotes.tsx
+++ src/components/notesTab/allNotes.tsx
@@ -635,32 +635,7 @@ const AllNotesTab: FC<Props> = ({ data, getNotes }) => {
       dataIndex: 'tags',
       align: 'left',
       id: 1,
-      render: ({ tags }) => (
-        <>
-          {' '}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            width="25px"
-            height="25px"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M9.568 3H5.25A2.25 2.25 0 003 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 005.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 009.568 3z"
-            />
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M6 6h.008v.008H6V6z"
-            />
-          </svg>
-          {tags}
-        </>
-      )
+      render: ({ tags }) => <>{tags}</>
     },
     {
       key: 'status',
