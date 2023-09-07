68654a4e244d18178e6194da2097567c9bda7d93
goldvlady <vladyslavabramov@outlook.com>
2023-09-07 18:40:59 +0100

    chat fix

--- src/views/Dashboard/DocChat/chat.tsx
+++ src/views/Dashboard/DocChat/chat.tsx
@@ -258,11 +258,11 @@ const Chat = ({
     // Adjust border radius based on inputValue
     if (inputValue.length > 0) {
       textAreaRef.current.style.borderRadius = '16px';
-      textAreaRef.current.style.maxheight = '2.5rem';
+      textAreaRef.current.style.minHeight = '2.5rem';
     } else {
       textAreaRef.current.style.borderRadius = '100px'; // Set initial border radius
     }
-  }, [inputValue]);
+  }, [inputValue, HomeWorkHelp]);
 
   return (
     <>
@@ -319,28 +319,6 @@ const Chat = ({
                 </div>
 
                 <GridContainer isHomeWorkHelp={HomeWorkHelp}>
-                  {HomeWorkHelp && messages && messages.length < 1 && (
-                    <div
-                      style={{
-                        position: 'absolute',
-                        top: '210px',
-                        right: '36%',
-                        /* width: 100%; */
-                        zIndex: '111111111'
-                      }}
-                    >
-                      <StyledDiv
-                        onClick={handleAceHomeWorkHelp}
-                        style={{
-                          color: '#FB8441',
-                          background: 'white'
-                        }}
-                        needIndex
-                      >
-                        Start New Conversation
-                      </StyledDiv>
-                    </div>
-                  )}
                   {isFindTutor && (
                     <OptionsContainer>
                       <Text className="">What do you need?</Text>
@@ -469,68 +447,35 @@ const Chat = ({
             </PillsContainer>
           </DownPillContainer>
         )}
-        {HomeWorkHelp && !!messages?.length && (
-          <ChatbotContainer chatbotSpace={chatbotSpace}>
-            <InputContainer>
-              <Input
-                ref={textAreaRef}
-                placeholder={
-                  HomeWorkHelp
-                    ? homeWorkHelpPlaceholder
-                    : `Ask Shepherd about ${snip(title, 40)}`
-                }
-                value={inputValue}
-                onKeyDown={handleKeyDown}
-                disabled={!isReadyToChat}
-                onChange={handleInputChange}
-                style={{
-                  minHeight: '2.5rem',
-                  maxHeight: '10rem',
-                  overflowY: 'auto'
-                }}
-              />
-              <SendButton type="button" onClick={handleSendMessage}>
-                <img alt="" src="/svgs/send.svg" className="w-8 h-8" />
-              </SendButton>
-            </InputContainer>
-            {/* {!HomeWorkHelp && (
+        <ChatbotContainer chatbotSpace={chatbotSpace}>
+          <InputContainer>
+            <Input
+              ref={textAreaRef}
+              placeholder={
+                HomeWorkHelp
+                  ? homeWorkHelpPlaceholder
+                  : `Ask Shepherd about ${snip(title, 40)}`
+              }
+              value={inputValue}
+              onKeyDown={handleKeyDown}
+              disabled={!isReadyToChat}
+              onChange={handleInputChange}
+              style={{
+                minHeight: '2.5rem',
+                maxHeight: '10rem',
+                overflowY: 'auto'
+              }}
+            />
+            <SendButton type="button" onClick={handleSendMessage}>
+              <img alt="" src="/svgs/send.svg" className="w-8 h-8" />
+            </SendButton>
+          </InputContainer>
+          {/* {!HomeWorkHelp && (
             <ClockButton type="button" onClick={onChatHistory}>
               <img alt="" src="/svgs/anti-clock.svg" className="w-5 h-5" />
             </ClockButton>
           )} */}
-          </ChatbotContainer>
-        )}
-        {!HomeWorkHelp && (
-          <ChatbotContainer chatbotSpace={chatbotSpace}>
-            <InputContainer>
-              <Input
-                ref={textAreaRef}
-                placeholder={
-                  HomeWorkHelp
-                    ? homeWorkHelpPlaceholder
-                    : `Ask Shepherd about ${documentId}`
-                }
-                value={inputValue}
-                onKeyDown={handleKeyDown}
-                disabled={!isReadyToChat}
-                onChange={handleInputChange}
-                style={{
-                  minHeight: '2.5rem',
-                  maxHeight: '10rem',
-                  overflowY: 'auto'
-                }}
-              />
-              <SendButton type="button" onClick={handleSendMessage}>
-                <img alt="" src="/svgs/send.svg" className="w-8 h-8" />
-              </SendButton>
-            </InputContainer>
-            {/* {!HomeWorkHelp && (
-            <ClockButton type="button" onClick={onChatHistory}>
-              <img alt="" src="/svgs/anti-clock.svg" className="w-5 h-5" />
-            </ClockButton>
-          )} */}
-          </ChatbotContainer>
-        )}
+        </ChatbotContainer>
       </Form>
 
       <CustomSideModal onClose={onClose} isOpen={isModalOpen}>
