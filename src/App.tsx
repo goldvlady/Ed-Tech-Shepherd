b65a8a246090a39f829c2bb12f25f2d289114108
goldvlady <vladyslavabramov@outlook.com>
2023-12-17 21:09:53 +0100

    study plan create

--- src/App.tsx
+++ src/App.tsx
@@ -74,6 +74,7 @@ import {
 } from 'react-router-dom';
 import 'stream-chat-react/dist/scss/v2/index.scss';
 import { ThemeProvider } from 'styled-components';
+import StudyPlans from './views/Dashboard/StudyPlans';
 
 const AuthAction = (props: any) => {
   const [params] = useSearchParams();
@@ -138,6 +139,7 @@ const studentRoutes = [
   { path: 'flashcards/:flashcardId', element: <FlashCard /> },
   { path: 'flashcards/:id/edit', element: <EditFlashCard /> },
   { path: 'library', element: <Library /> },
+  { path: 'study-plans', element: <StudyPlans /> },
   // quizzes
   { path: 'quizzes', element: <Quizzes /> },
   { path: 'quizzes/create', element: <CreateQuizzes /> },
