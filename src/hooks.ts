f1af67067bb1167265b9dd55b7a04c0f82f127a4
goldvlady <vladyslavabramov@outlook.com>
2024-02-03 11:01:55 +0530

    Created custom hook for copy to clipboard

--- src/hooks.ts
+++ src/hooks.ts
@@ -49,3 +49,27 @@ export function useSearchQuery() {
 
   return React.useMemo(() => new URLSearchParams(search), [search]);
 }
+
+export function useCopyToClipboard({ timeout = 2000 }: { timeout?: number }) {
+  const [isCopied, setIsCopied] = React.useState<Boolean>(false);
+
+  const copyToClipboard = (value: string) => {
+    if (typeof window === 'undefined' || !navigator.clipboard?.writeText) {
+      return;
+    }
+
+    if (!value) {
+      return;
+    }
+
+    navigator.clipboard.writeText(value).then(() => {
+      setIsCopied(true);
+
+      setTimeout(() => {
+        setIsCopied(false);
+      }, timeout);
+    });
+  };
+
+  return { isCopied, copyToClipboard };
+}
