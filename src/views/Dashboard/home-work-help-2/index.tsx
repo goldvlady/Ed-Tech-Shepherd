9fc6bff4077c36f5a299b39172e50f0175a68781
goldvlady <vladyslavabramov@outlook.com>
2024-02-06 21:16:28 +0530

    Fixed scroll issue for conversation history

--- src/views/Dashboard/home-work-help-2/index.tsx
+++ src/views/Dashboard/home-work-help-2/index.tsx
@@ -3,7 +3,7 @@ import ChatHistory from './_components/chat-history';
 function HomeWorkHelp2() {
   return (
     <div className="w-full h-full flex gap-2">
-      <div className="h-full flex-[3.5] hidden md:block min-w-[15em] border-r">
+      <div className="h-full max-h-screen overflow-y-scroll flex-[3.5] hidden md:block min-w-[15em] border-r">
         <ChatHistory />
       </div>
       <div className="h-full flex-[6.5]">
