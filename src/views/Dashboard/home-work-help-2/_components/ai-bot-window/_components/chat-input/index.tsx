8547dcdc305cffb4d72a30ee0c6458202aa8d8d8
goldvlady <vladyslavabramov@outlook.com>
2024-02-06 22:00:19 +0530

    Created basic input and submit button

--- src/views/Dashboard/home-work-help-2/_components/ai-bot-window/_components/chat-input/index.tsx
+++ src/views/Dashboard/home-work-help-2/_components/ai-bot-window/_components/chat-input/index.tsx
@@ -1,6 +1,13 @@
+import { Input } from '@chakra-ui/input';
+
 function ChatInput() {
   return (
-    <div className="w-full h-10 border rounded-sm shadow-sm">Chat Input</div>
+    <div className="w-full border rounded-sm shadow-sm flex gap-2 items-center">
+      <Input size="md" placeholder="Ask me anything?" />
+      <button className="bg-blue-500 text-white px-4 py-2 rounded-sm">
+        Send
+      </button>
+    </div>
   );
 }
 
