b6abfdb6b42a98199c0bc2cdefb235fab81fdc81
goldvlady <vladyslavabramov@outlook.com>
2024-02-06 22:03:50 +0530

    Created empty window for chat messages

--- src/views/Dashboard/home-work-help-2/_components/ai-bot-window/_components/conversation-window/index.tsx
+++ src/views/Dashboard/home-work-help-2/_components/ai-bot-window/_components/conversation-window/index.tsx
@@ -1,21 +1,20 @@
 import ChatItem from './chat-item';
 
 const ConversationWindow = () => {
+  const messages = [];
   return (
     <div className="flex-1 h-full overflow-y-scroll flex gap-3 w-full flex-col pr-4">
-      <ChatItem type="user" message="Hello! How are you doing?" />
-      <ChatItem
-        type="bot"
-        message="Hello! I am doing great. How can I help you?"
-      />
-      <ChatItem type="user" message="I need help with my homework." />
-      <ChatItem type="bot" message="Sure! I can help you with that." />
-      <ChatItem type="user" message="I am stuck on this question." />
-      <ChatItem type="bot" message="What is the question?" />
-      <ChatItem type="user" message="The question is 2+2." />
-      <ChatItem type="bot" message="The answer is 4." />
-      <ChatItem type="user" message="Thank you!" />
-      <ChatItem type="bot" message="You're welcome!" />
+      {messages.length > 0 ? 'Show messages' : <EmptyChat />}
+    </div>
+  );
+};
+
+const EmptyChat = () => {
+  return (
+    <div className="flex-1 h-full overflow-y-scroll flex gap-3 w-full flex-col pr-4">
+      <div className="flex w-full h-full items-center justify-center">
+        <p className="text-gray-400">No messages yet</p>
+      </div>
     </div>
   );
 };
