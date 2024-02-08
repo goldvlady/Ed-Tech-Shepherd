156e4285d438a3f99ee2f719c7ba7eb8c5575596
goldvlady <vladyslavabramov@outlook.com>
2024-02-08 21:49:36 +0530

    Created dummy file uploader button

--- src/views/Dashboard/home-work-help-2/_components/ai-bot-window/chat-room/index.tsx
+++ src/views/Dashboard/home-work-help-2/_components/ai-bot-window/chat-room/index.tsx
@@ -67,17 +67,22 @@ const PromptInput = () => {
           Find a tutor
         </Button>
       </div>
-      <div className="input-box flex gap-2 flex-col bg-white rounded-md shadow-md w-full">
-        <div className="input-element w-full flex-1 px-3">
+      <div className="input-box h-[85px] flex gap-2 flex-col bg-white rounded-md shadow-md w-full px-2 pb-2">
+        <div className="input-element w-full flex-1">
           <input
             type="text"
             className="w-full input flex-1 py-2 border-none bg-transparent outline-none active:outline-none active:ring-0 border-transparent focus:border-transparent focus:ring-0 placeholder:text-[#CDD1D5] placeholder:font-normal text-[#6E7682] font-normal"
             placeholder="How can Shepherd help with your homework?"
           />
         </div>
-        <div className="file-uploader-submit-section flex-1 flex justify-between py-1 px-4">
-          <div className="file-uploader">
-            <input type="file" disabled />
+        <div className="file-uploader-submit-section flex-1 flex justify-between px-2.5">
+          <div className="file-uploader flex gap-[1px]">
+            <button className="flex items-center justify-center w-[28px] h-[28px] rounded-tl-md rounded-bl-md bg-[#F9F9FB]">
+              <ShareIcon />
+            </button>
+            {/* <span className="text-[#969CA6] bg-[#F9F9FB] font-normal h-[28px] text-[10px] flex items-center px-2">
+              <span>File.txt</span>
+            </span> */}
           </div>
           <div className="submit-button">
             <button>Send</button>
