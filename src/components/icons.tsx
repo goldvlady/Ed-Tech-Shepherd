22583986cc6ec11d800b83039ff0c5e7cff85120
goldvlady <vladyslavabramov@outlook.com>
2024-02-08 21:59:05 +0530

    Moved arrow icon to separate file

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,3 +1,28 @@
+export const RightArrowIcon = () => {
+  return (
+    <svg
+      width="13"
+      height="11"
+      viewBox="0 0 13 11"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M1 5.36353L10 5.36353"
+        stroke="white"
+        stroke-width="1.52728"
+        stroke-linecap="round"
+      />
+      <path
+        d="M6.54533 9.72727L11.5739 5.70438C11.7923 5.52969 11.7923 5.19758 11.5739 5.02289L6.54533 1"
+        stroke="white"
+        stroke-width="1.63637"
+        stroke-linecap="round"
+      />
+    </svg>
+  );
+};
+
 export const ShareIcon = () => {
   return (
     <svg
