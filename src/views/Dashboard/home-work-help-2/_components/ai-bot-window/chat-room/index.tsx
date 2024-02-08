71a645e00594c8f0ae406ffc6c6a450b9dbf76c4
goldvlady <vladyslavabramov@outlook.com>
2024-02-08 16:59:32 +0530

    Created basic prompt input

--- src/views/Dashboard/home-work-help-2/_components/ai-bot-window/chat-room/index.tsx
+++ src/views/Dashboard/home-work-help-2/_components/ai-bot-window/chat-room/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@chakra-ui/button';
 import { ShareIcon } from '../../../../../../components/icons';
 import ChatMessage from './_components/chat-message';
 
@@ -39,10 +40,46 @@ function ChatRoom() {
             type="bot"
           />
         </div>
-        <footer className="h-[127px] w-full">I am footer</footer>
+        <footer className=" w-full flex justify-center mb-6">
+          <PromptInput />
+        </footer>
       </div>
     </div>
   );
 }
+
+const PromptInput = () => {
+  return (
+    <div className="w-full h-full flex gap-5 flex-col items-center justify-center max-w-[600px]">
+      <div className="find-tutor-button flex justify-end w-full">
+        <Button
+          variant="outline"
+          borderRadius="full"
+          size="md"
+          colorScheme="blue"
+        >
+          Find a tutor
+        </Button>
+      </div>
+      <div className="input-box flex gap-2 flex-col bg-white rounded-md shadow-md w-full">
+        <div className="input-element w-full flex-1 px-3">
+          <input
+            type="text"
+            className="w-full input flex-1 py-2 border-none bg-transparent outline-none active:outline-none active:ring-0 border-transparent focus:border-transparent focus:ring-0 placeholder:text-[#CDD1D5] placeholder:font-normal text-[#6E7682] font-normal"
+            placeholder="How can Shepherd help with your homework?"
+          />
+        </div>
+        <div className="file-uploader-submit-section flex-1 flex justify-between py-1 px-4">
+          <div className="file-uploader">
+            <input type="file" disabled />
+          </div>
+          <div className="submit-button">
+            <button>Send</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default ChatRoom;
