8842f3d461d3e4d5530143deef0cc2a2d2abe468
goldvlady <vladyslavabramov@outlook.com>
2024-02-09 20:49:09 +0530

    Added info log at home-work component

--- src/views/Dashboard/home-work-help-2/index.tsx
+++ src/views/Dashboard/home-work-help-2/index.tsx
@@ -3,7 +3,7 @@ import AiChatBotWindow from './_components/ai-bot-window';
 import ChatHistory from './_components/chat-history';
 
 const Component = () => {
-  console.log('I AM PARENT');
+  console.log('I AM PARENT, I dont re-render on URL change, I am memoized.');
   return (
     <div className="w-full h-full flex">
       <div className="h-full max-h-screen hidden md:block w-[348px] border-r">
