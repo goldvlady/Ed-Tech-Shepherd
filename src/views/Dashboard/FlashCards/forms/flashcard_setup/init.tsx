4c780b5660256c9ff3665bc9afb33349a058362f
goldvlady <vladyslavabramov@outlook.com>
2024-02-14 14:51:47 +0000

    trigger env reset

--- src/views/Dashboard/FlashCards/forms/flashcard_setup/init.tsx
+++ src/views/Dashboard/FlashCards/forms/flashcard_setup/init.tsx
@@ -56,6 +56,7 @@ const FlashCardSetupInit = ({
   const [localData, setLocalData] = useState<typeof flashcardData>(dummyData); // A local state for storing user inputs
   const [togglePlansModal, setTogglePlansModal] = useState(false);
   const [plansModalMessage, setPlansModalMessage] = useState('');
+
   const [plansModalSubMessage, setPlansModalSubMessage] = useState('');
 
   useEffect(() => {
