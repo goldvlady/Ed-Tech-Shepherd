a5bb1b2b3b49ba2a5cf5d84a9af25651e96b4c22
goldvlady <vladyslavabramov@outlook.com>
2024-02-21 15:58:12 +0100

    feat: create providerList component

---
+++ src/views/Dashboard/Library/components/ProviderList.tsx
@@ -0,0 +1,32 @@
+import LoaderOverlay from '../../../../components/loaderOverlay';
+import TitleCard from './TitleCard';
+import { SimpleGrid } from '@chakra-ui/react';
+import React from 'react';
+
+interface LibraryProviderProps {
+  providers: Array<{ _id: string; name: string }>;
+  onSelectProvider: (providerId: string) => void;
+}
+
+const ProviderList: React.FC<LibraryProviderProps> = ({
+  providers,
+  onSelectProvider
+}) => {
+  if (!providers?.length) {
+    return <LoaderOverlay />;
+  }
+
+  return (
+    <SimpleGrid columns={{ base: 1, md: 2, lg: 2, xl: 3 }} spacing={10}>
+      {providers.map((provider) => (
+        <TitleCard
+          key={provider._id}
+          data={{ name: provider.name }}
+          onClick={() => onSelectProvider(provider._id)}
+        />
+      ))}
+    </SimpleGrid>
+  );
+};
+
+export default ProviderList;
