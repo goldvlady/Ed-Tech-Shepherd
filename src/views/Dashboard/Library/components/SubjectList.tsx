287f4109f46ec75541f12abe932f635cd5171228
goldvlady <vladyslavabramov@outlook.com>
2024-02-24 00:03:20 +0100

    feat: add empty state return to DeckList, SubjectList, TopicList

--- src/views/Dashboard/Library/components/SubjectList.tsx
+++ src/views/Dashboard/Library/components/SubjectList.tsx
@@ -1,6 +1,6 @@
 import LoaderOverlay from '../../../../components/loaderOverlay';
 import TitleCard from './TitleCard';
-import { SimpleGrid } from '@chakra-ui/react';
+import { SimpleGrid, Box, Text } from '@chakra-ui/react';
 import React from 'react';
 
 interface LibrarySubjectProps {
@@ -13,7 +13,39 @@ const SubjectList: React.FC<LibrarySubjectProps> = ({
   onSelectSubject
 }) => {
   if (!subjects?.length) {
-    return <LoaderOverlay />;
+    return (
+      <Box
+        display={'flex'}
+        flexDirection={'column'}
+        justifyContent={'start'}
+        height={'calc(100vh - 350px)'}
+      >
+        <Box
+          width={'100%'}
+          display={'flex'}
+          height="100%"
+          justifyContent={'center'}
+          flexDirection={'column'}
+          alignItems={'center'}
+        >
+          <img
+            src="/images/empty_illustration.svg"
+            alt="empty directory icon"
+          />
+          <Text
+            color="text.300"
+            fontFamily="Inter"
+            fontSize="16px"
+            fontStyle="normal"
+            fontWeight="500"
+            lineHeight="21px"
+            letterSpacing="0.112px"
+          >
+            No cards to display
+          </Text>
+        </Box>
+      </Box>
+    );
   }
 
   return (
--- src/views/Dashboard/Library/components/TopicList.tsx
+++ src/views/Dashboard/Library/components/TopicList.tsx
@@ -1,7 +1,7 @@
 import LoaderOverlay from '../../../../components/loaderOverlay';
 import libraryTopicStore from '../../../../state/libraryTopicStore';
 import TitleCard from './TitleCard';
-import { SimpleGrid } from '@chakra-ui/react';
+import { SimpleGrid, Box, Text } from '@chakra-ui/react';
 import React, { useEffect } from 'react';
 
 interface LibraryTopicProps {
@@ -21,11 +21,38 @@ const TopicList: React.FC<LibraryTopicProps> = ({
     }
   }, [subjectId, fetchlibraryTopics]);
 
-  if (!libraryTopics?.length) {
+  if (isLoading && !libraryTopics?.length) {
     return <LoaderOverlay />;
   }
   return !libraryTopics?.length ? (
-    <LoaderOverlay />
+    <Box
+      display={'flex'}
+      flexDirection={'column'}
+      justifyContent={'start'}
+      height={'calc(100vh - 350px)'}
+    >
+      <Box
+        width={'100%'}
+        display={'flex'}
+        height="100%"
+        justifyContent={'center'}
+        flexDirection={'column'}
+        alignItems={'center'}
+      >
+        <img src="/images/empty_illustration.svg" alt="empty directory icon" />
+        <Text
+          color="text.300"
+          fontFamily="Inter"
+          fontSize="16px"
+          fontStyle="normal"
+          fontWeight="500"
+          lineHeight="21px"
+          letterSpacing="0.112px"
+        >
+          No cards to display
+        </Text>
+      </Box>
+    </Box>
   ) : (
     <SimpleGrid columns={{ base: 1, md: 2, lg: 2, xl: 3 }} spacing={10}>
       {libraryTopics.map((topic) => (
