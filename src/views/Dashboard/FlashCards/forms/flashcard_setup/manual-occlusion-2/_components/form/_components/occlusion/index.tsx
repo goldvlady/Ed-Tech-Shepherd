706048efa44c9b4fb0a01a48dabc0514c02465a2
goldvlady <vladyslavabramov@outlook.com>
2024-02-27 11:15:07 +0530

    Minor improvements in occl. card

--- src/views/Dashboard/FlashCards/forms/flashcard_setup/manual-occlusion-2/_components/form/_components/occlusion/index.tsx
+++ src/views/Dashboard/FlashCards/forms/flashcard_setup/manual-occlusion-2/_components/form/_components/occlusion/index.tsx
@@ -1,3 +1,4 @@
+import { ResetIcon } from '@radix-ui/react-icons';
 import { Button } from '../../../../../../../../../../components/ui/button';
 import {
   Dialog,
@@ -31,18 +32,24 @@ function Occlusion({
       }}
     >
       <DialogContent className="bg-white p-0 flex flex-col w-[894px] max-w-4xl">
-        <header className="flex p-2 justify-between">
-          <div className="flex gap-4 items-center">
-            <span>Undo</span>
-            <span>Redo</span>
+        <header className="flex p-4 justify-between">
+          <div className="flex gap-1 items-center">
+            <span className='px-1 py-0.5 rounded bg-[#F3F5F6] cursor-pointer'>
+              <ResetIcon className="w-4 h-4 hover:scale-105" />
+            </span>
+            <span className='px-1 py-0.5 rounded bg-[#F3F5F6] cursor-pointer scale-x-[-1]'>
+              <ResetIcon className="w-4 h-4 hover:scale-105" />
+            </span>
           </div>
           <button
+            className="w-[60px] flex items-center justify-center gap-1 rounded-full bg-[#F3F5F6] text-[#969CA6] text-xs'"
             onClick={() => {
               resetForm();
               close();
             }}
           >
-            Close
+            <span className="text-[#969CA6] text-xs">Close</span>
+            <span className="text-[#969CA6] text-xs"> &times;</span>
           </button>
         </header>
 
@@ -52,8 +59,8 @@ function Occlusion({
           setElements={setElements}
         />
 
-        <footer className="flex justify-between w-full p-2">
-          {/* <div>Resize</div> */}
+        <footer className="flex justify-between w-full p-4">
+          <div className='text-xs opacity-50'>Resize</div>
           <Button className="bg-blue-600 text-white" onClick={handleSubmit}>
             Save
           </Button>
