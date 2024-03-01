3fc7079376d6aaa10c50e65abbd8148b01750b87
goldvlady <vladyslavabramov@outlook.com>
2024-03-01 08:38:41 +0100

    feat: improve form design and add resetForm on success

--- src/views/ActivateTutor.tsx
+++ src/views/ActivateTutor.tsx
@@ -3,10 +3,11 @@ import {
   Box,
   Button,
   FormControl,
-  FormErrorMessage,
   FormLabel,
   Input,
-  VStack
+  VStack,
+  Heading,
+  FormErrorMessage
 } from '@chakra-ui/react';
 import { Field, Form, Formik } from 'formik';
 import React from 'react';
@@ -22,91 +23,98 @@ const ActivateTutor: React.FC = () => {
   const toast = useCustomToast();
 
   return (
-    <Box p={5}>
-      <Formik
-        initialValues={{ tutorEmail: '', apiKey: '' }}
-        validationSchema={ActivateTutorSchema}
-        onSubmit={async (values, actions) => {
-          try {
-            const response = await ApiService.activateTutor({
-              apiKey: values.apiKey,
-              tutorEmail: values.tutorEmail
-            });
-            if (response.status === 200) {
+    <Box display="flex" justifyContent="center" mt="5%">
+      <Box width="33%" p={5} boxShadow="md" borderRadius="md">
+        <Heading as="h2" size="lg" textAlign="center" mb={5}>
+          Activate Tutor
+        </Heading>
+        <Formik
+          initialValues={{ tutorEmail: '', apiKey: '' }}
+          validationSchema={ActivateTutorSchema}
+          onSubmit={async (values, { setSubmitting, resetForm }) => {
+            try {
+              const response = await ApiService.activateTutor({
+                apiKey: values.apiKey,
+                tutorEmail: values.tutorEmail
+              });
+              if (response.status === 200) {
+                toast({
+                  title: 'Tutor activated successfully',
+                  position: 'top-right',
+                  status: 'success',
+                  duration: 5000,
+                  isClosable: true
+                });
+
+                resetForm();
+              } else {
+                toast({
+                  title: 'Something went wrong..',
+                  status: 'error',
+                  position: 'top-right',
+                  isClosable: true
+                });
+              }
+            } catch (error) {
               toast({
-                title: 'Tutor activated successfully',
+                title: 'Failed to activate tutor',
                 position: 'top-right',
-                status: 'success',
+                status: 'error',
                 duration: 5000,
                 isClosable: true
               });
-            } else {
-              toast({
-                title: 'Something went wrong..',
-                status: 'error',
-                position: 'top-right',
-                isClosable: true
-              });
             }
-          } catch (error) {
-            toast({
-              title: 'Failed to activate tutor',
-              position: 'top-right',
-              status: 'error',
-              duration: 5000,
-              isClosable: true
-            });
-          }
-          actions.setSubmitting(false);
-        }}
-      >
-        {({ isSubmitting }) => (
-          <Form>
-            <VStack spacing={4}>
-              <Field name="tutorEmail">
-                {({ field, form }) => (
-                  <FormControl
-                    isInvalid={
-                      form.errors.tutorEmail && form.touched.tutorEmail
-                    }
-                  >
-                    <FormLabel htmlFor="tutorEmail">Tutor Email</FormLabel>
-                    <Input
-                      {...field}
-                      id="tutorEmail"
-                      placeholder="Tutor Email"
-                    />
-                    <FormErrorMessage>
-                      {form.errors.tutorEmail}
-                    </FormErrorMessage>
-                  </FormControl>
-                )}
-              </Field>
+            setSubmitting(false);
+          }}
+        >
+          {({ isSubmitting }) => (
+            <Form>
+              <VStack spacing={4}>
+                <Field name="tutorEmail">
+                  {({ field, form }) => (
+                    <FormControl
+                      isInvalid={
+                        form.errors.tutorEmail && form.touched.tutorEmail
+                      }
+                    >
+                      <FormLabel htmlFor="tutorEmail">Tutor Email</FormLabel>
+                      <Input
+                        {...field}
+                        id="tutorEmail"
+                        placeholder="Tutor Email"
+                      />
+                      <FormErrorMessage>
+                        {form.errors.tutorEmail}
+                      </FormErrorMessage>
+                    </FormControl>
+                  )}
+                </Field>
 
-              <Field name="apiKey">
-                {({ field, form }) => (
-                  <FormControl
-                    isInvalid={form.errors.apiKey && form.touched.apiKey}
-                  >
-                    <FormLabel htmlFor="apiKey">API Key</FormLabel>
-                    <Input {...field} id="apiKey" placeholder="API Key" />
-                    <FormErrorMessage>{form.errors.apiKey}</FormErrorMessage>
-                  </FormControl>
-                )}
-              </Field>
+                <Field name="apiKey">
+                  {({ field, form }) => (
+                    <FormControl
+                      isInvalid={form.errors.apiKey && form.touched.apiKey}
+                    >
+                      <FormLabel htmlFor="apiKey">API Key</FormLabel>
+                      <Input {...field} id="apiKey" placeholder="API Key" />
+                      <FormErrorMessage>{form.errors.apiKey}</FormErrorMessage>
+                    </FormControl>
+                  )}
+                </Field>
 
-              <Button
-                mt={4}
-                colorScheme="teal"
-                isLoading={isSubmitting}
-                type="submit"
-              >
-                Activate Tutor
-              </Button>
-            </VStack>
-          </Form>
-        )}
-      </Formik>
+                <Button
+                  mt={4}
+                  colorScheme="teal"
+                  isLoading={isSubmitting}
+                  type="submit"
+                >
+                  Activate Tutor
+                </Button>
+              </VStack>
+            </Form>
+          )}
+        </Formik>
+      </Box>
     </Box>
   );
 };
