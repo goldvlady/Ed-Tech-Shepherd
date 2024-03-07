359df459ce6349507137433d27a02c08f5927087
goldvlady <vladyslavabramov@outlook.com>
2024-03-07 23:53:06 +0000

    use store conversation

--- src/views/Dashboard/StudyPlans/components/topics.tsx
+++ src/views/Dashboard/StudyPlans/components/topics.tsx
@@ -28,11 +28,7 @@ import {
   HStack,
   useDisclosure,
   UnorderedList,
-  ListItem,
-  PopoverContent,
-  PopoverTrigger,
-  Popover,
-  CircularProgress
+  ListItem
 } from '@chakra-ui/react';
 import useInitializeAIChat from '../hooks/useInitializeAITutor';
 import ResourceIcon from '../../../../assets/resources-plan.svg';
@@ -74,7 +70,7 @@ function Topics(props) {
 
   const [showNoteModal, setShowNoteModal] = useState(false);
   const [convoId, setConvoId] = useState(null);
-  const [initializing, setInitializing] = useState(false);
+
   const [state, setState] = useState({
     // studyPlans: storePlans,
     isPageLoading: false,
@@ -121,7 +117,11 @@ function Topics(props) {
   const groupedTopics = planTopics?.schedules.reduce((grouped, topic) => {
     let testDate;
     if (topic.topicMetaData && topic.topicMetaData.length > 0) {
-      testDate = new Date(topic.topicMetaData[0].testDate).toDateString();
+      if (topic.topicMetaData[0]?.testDate) {
+        testDate = new Date(topic.topicMetaData[0].testDate).toDateString();
+      } else {
+        testDate = new Date(topic.endDate).toDateString();
+      }
     } else {
       testDate = new Date(topic.endDate).toDateString();
     }
@@ -383,29 +383,7 @@ function Topics(props) {
       navigateOnInitialized: true,
       onInitialized: saveStudyPlanMetaData
     });
-    // const handleInitializeAiTutor = async () => {
-    //   setInitializing(true);
-    //   try {
-    //     await initializeAItutor({
-    //       topic: topic.topicDetails?.label,
-    //       subject: getSubject(planTopics.course),
-    //       level: 'Sophomore',
-    //       studentId: user?._id,
-    //       firebaseId: user?.firebaseId,
-    //       namespace: 'homework-help'
-    //     });
-    //   } catch (error) {
-    //     toast({
-    //       title: 'Error initializing AI Tutor',
-    //       position: 'top-right',
-    //       status: 'error',
-    //       isClosable: true
-    //     });
-    //     // console.error('Error initializing AI Tutor:', error);
-    //   } finally {
-    //     setInitializing(false);
-    //   }
-    // };
+
     return (
       <Box
         bg="white"
@@ -531,7 +509,7 @@ function Topics(props) {
             <VStack
               cursor={'pointer'}
               onClick={() => {
-                setInitializing(true);
+                console.log(topic.topicDetails?.label, topic.topicDetails);
                 initializeAItutor({
                   topic: topic.topicDetails?.label,
                   subject: getSubject(planTopics.course),
@@ -540,7 +518,11 @@ function Topics(props) {
                   firebaseId: user?.firebaseId,
                   namespace: 'homework-help'
                 });
-                setInitializing(false);
+                // navigate(
+                //   `/dashboard/ace-homework?subject=${getSubject(
+                //     planTopics.course
+                //   )}&topic=${topic.topicDetails?.label}`
+                // );
               }}
             >
               <AiTutorIcon />
@@ -548,6 +530,38 @@ function Topics(props) {
                 AI Tutor
               </Text>
             </VStack>
+
+            <Menu isLazy>
+              <MenuButton>
+                {' '}
+                <VStack>
+                  <AiTutorIcon />
+                  <Text fontSize={12} fontWeight={500}>
+                    AI Tutor
+                  </Text>
+                </VStack>
+              </MenuButton>
+              <MenuList
+                maxH={60}
+                overflowY="scroll"
+                bg="white"
+                border="1px solid #E2E8F0"
+                borderRadius="md"
+              >
+                {studyPlanResources && (
+                  <>
+                    {/* <Text fontSize={12}>Initialising conversation...</Text> */}
+                    <Button
+                      variant={'ghost'}
+                      fontSize={12}
+                      // onClick={handleStartConversation}
+                    >
+                      Start New Conversation
+                    </Button>
+                  </>
+                )}
+              </MenuList>
+            </Menu>
 
             <Menu isLazy>
               <MenuButton>
@@ -622,7 +636,6 @@ function Topics(props) {
               </MenuList>
             </Menu>
             <VStack
-              cursor={'pointer'}
               onClick={() => {
                 updateState({
                   selectedTopic: topic._id
