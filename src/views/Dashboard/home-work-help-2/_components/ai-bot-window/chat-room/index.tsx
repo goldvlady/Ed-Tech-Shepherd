dfd41526edeee7353a1206cd7d58a578d46ebc2a
goldvlady <vladyslavabramov@outlook.com>
2024-03-21 10:47:51 +0100

    husky

--- src/views/Dashboard/home-work-help-2/_components/ai-bot-window/chat-room/index.tsx
+++ src/views/Dashboard/home-work-help-2/_components/ai-bot-window/chat-room/index.tsx
@@ -74,7 +74,6 @@ function ChatRoom() {
           throw new Error('Network response was not ok');
         }
 
-
         // Create a new EventSource instance to handle server sent events
         const eventSource = new EventSource(
           `${process.env.REACT_APP_AI_II}/maths/`
