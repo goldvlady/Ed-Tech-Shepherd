2d34cbf868840f6be040912cc19639fe49fc62ca
goldvlady <vladyslavabramov@outlook.com>
2024-03-26 10:09:24 +0100

    set user name to JD if it does not exist

--- src/views/Dashboard/home-work-help-2/_components/ai-bot-window/hooks/useChatManager.tsx
+++ src/views/Dashboard/home-work-help-2/_components/ai-bot-window/hooks/useChatManager.tsx
@@ -356,7 +356,7 @@ const useChatManager = (
       if (socketRef.current) {
         socketRef.current.disconnect(); // Disconnect existing socket
       }
-      queryParams.name = user.name.first;
+      queryParams.name = user ? user.name.first : 'John Doe';
       // Initialize new socket connection with server
       socketRef.current = io(SERVER_URL + '/' + namespace, {
         extraHeaders: {
@@ -404,7 +404,7 @@ const useChatManager = (
       conversationOptions?: InitConversationOptions
     ) => {
       const { conversationId } = queryParams;
-      queryParams.name = user.name.first;
+      queryParams.name = user ? user.name.first : 'John Doe';
       refreshManager();
       if (conversationOptions) {
         setChatWindowParams({
