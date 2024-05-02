b848efe5dea97e451ff10af82d1487a2e7b9bb6f
goldvlady <vladyslavabramov@outlook.com>
2024-05-02 20:38:56 +0530

    Improved document list in doc chat window

--- src/views/Dashboard/multi-rag/_components/docs-thumbnail-list/_components/thumbnail-list/index.tsx
+++ src/views/Dashboard/multi-rag/_components/docs-thumbnail-list/_components/thumbnail-list/index.tsx
@@ -1,10 +1,14 @@
+import { PlusIcon } from 'lucide-react';
 import Thumbnail from '../thumbnail';
 
 function ThumbnailList({ fetchedDocuments }: { fetchedDocuments: any[] }) {
   return (
     <div className="w-full h-full mt-[1.5rem]">
-      <h5 className="text-[#585F68] text-[0.75rem] font-normal mb-[10px]">
-        Sources
+      <h5 className="text-[#585F68] text-[0.75rem] font-normal mb-[10px] flex justify-between">
+        Sources{' '}
+        <span className="w-[1.25rem] h-[1.25rem] bg-[#207DF7] rounded-full flex items-center justify-center cursor-pointer">
+          <PlusIcon className="text-white w-4" />
+        </span>
       </h5>
       <div className="thumbnail-list space-y-2 overflow-y-scroll h-full pb-40 no-scrollbar">
         {fetchedDocuments.map((item: any) => (
--- src/views/Dashboard/multi-rag/_components/docs-thumbnail-list/_components/thumbnail/index.tsx
+++ src/views/Dashboard/multi-rag/_components/docs-thumbnail-list/_components/thumbnail/index.tsx
@@ -1,7 +1,22 @@
+import { DotsHorizontalIcon } from '@radix-ui/react-icons';
+
+function truncateText(text, maxLength) {
+  if (text.length > maxLength) {
+    return text.slice(0, maxLength) + '...';
+  } else {
+    return text;
+  }
+}
+
 function Thumbnail({ data }: { data: any }) {
   return (
-    <div className="border h-[10.31rem] w-[10.31rem] rounded-[10px] bg-white">
-      {data.collection_name}
+    <div className="border h-[10.31rem] w-[10.31rem] rounded-[10px] bg-white relative p-[0.68rem] flex items-end">
+      <div className="w-[1.87rem] h-[1.87rem] absolute rounded-full bg-[#F9F9FB] top-0 right-0 m-[0.68rem] flex justify-center items-center cursor-pointer">
+        <DotsHorizontalIcon />
+      </div>
+      <p className="text-[#585F68] text-[10px]">
+        {truncateText(data.collection_name, 55)}
+      </p>
     </div>
   );
 }
