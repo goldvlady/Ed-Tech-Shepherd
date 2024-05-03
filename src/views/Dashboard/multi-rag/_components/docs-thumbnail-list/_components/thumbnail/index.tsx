f5d0d052de6721a03d267cd8cd35c7fd95440904
goldvlady <vladyslavabramov@outlook.com>
2024-05-03 18:38:52 +0530

    Enabled document selection from the document list

--- src/views/Dashboard/multi-rag/_components/docs-thumbnail-list/_components/thumbnail/index.tsx
+++ src/views/Dashboard/multi-rag/_components/docs-thumbnail-list/_components/thumbnail/index.tsx
@@ -1,22 +1,53 @@
 import { DotsHorizontalIcon } from '@radix-ui/react-icons';
+import { Switch } from '../../../../../../../components/ui/switch';
+import { Checkbox } from '../../../../../../../components/ui/checkbox';
+import { CheckIcon } from 'lucide-react';
+import { useState } from 'react';
+import { cn } from '../../../../../../../library/utils';
 
 function truncateText(text, maxLength) {
   if (text.length > maxLength) {
-    return text.slice(0, maxLength) + '...';
+    const extension = text.substring(text.lastIndexOf('.'));
+    return text.slice(0, maxLength - extension.length - 3) + '...' + extension;
   } else {
     return text;
   }
 }
 
 function Thumbnail({ data }: { data: any }) {
+  const [selected, setSelected] = useState(false);
+  // #EBF4FE
   return (
-    <div className="border h-[10.31rem] w-[10.31rem] rounded-[10px] bg-white relative p-[0.68rem] flex items-end">
+    <div
+      className={cn(
+        'border h-[10.31rem] w-[10.31rem] rounded-[10px] bg-white relative p-[0.68rem] flex items-end transition-colors',
+        {
+          'bg-[#EBF4FE]': selected
+        }
+      )}
+    >
       <div className="w-[1.87rem] h-[1.87rem] absolute rounded-full bg-[#F9F9FB] top-0 right-0 m-[0.68rem] flex justify-center items-center cursor-pointer">
         <DotsHorizontalIcon />
       </div>
-      <p className="text-[#585F68] text-[10px]">
-        {truncateText(data.collection_name, 55)}
-      </p>
+      <div className="flex items-center gap-1 justify-between w-full">
+        <p className="text-[#585F68] text-[10px] whitespace-nowrap">
+          {truncateText(data.collection_name, 25)}
+        </p>
+        <div
+          role="button"
+          onClick={() => {
+            setSelected(!selected);
+          }}
+          className={cn(
+            'w-[0.87rem] h-[0.87rem] rounded-[3px] bg-[#F9F9FB] flex justify-center items-center p-[2px] transition',
+            {
+              'bg-[#207DF7]': selected
+            }
+          )}
+        >
+          <CheckIcon className="text-white" />
+        </div>
+      </div>
     </div>
   );
 }
