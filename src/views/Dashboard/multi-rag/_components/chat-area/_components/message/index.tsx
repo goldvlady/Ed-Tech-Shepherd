75b46e8e901cc662f2c3e038a5f50d579e3f8fad
goldvlady <vladyslavabramov@outlook.com>
2024-05-14 19:53:54 +0530

    Made scrollbar hidden in citation dialog

--- src/views/Dashboard/multi-rag/_components/chat-area/_components/message/index.tsx
+++ src/views/Dashboard/multi-rag/_components/chat-area/_components/message/index.tsx
@@ -121,7 +121,7 @@ const Message = ({
                           <HoverCard.Portal>
                             <HoverCard.Content
                               side="top"
-                              className="overflow-y-scroll max-h-[300px] data-[side=bottom]:animate-slideUpAndFade data-[side=right]:animate-slideLeftAndFade data-[side=left]:animate-slideRightAndFade data-[side=top]:animate-slideDownAndFade w-[300px] rounded-md bg-white p-5 shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] data-[state=open]:transition-all"
+                              className="overflow-y-scroll max-h-[300px] data-[side=bottom]:animate-slideUpAndFade data-[side=right]:animate-slideLeftAndFade data-[side=left]:animate-slideRightAndFade data-[side=top]:animate-slideDownAndFade w-[300px] rounded-md bg-white p-5 shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] data-[state=open]:transition-all no-scrollbar"
                               sideOffset={5}
                             >
                               <p className="text-xs">{el.text}</p>
